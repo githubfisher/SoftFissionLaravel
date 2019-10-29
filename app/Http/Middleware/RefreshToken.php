9269b64b24d43decdd4ffc67c6a87c407ddfd4cf
githubfisher <gooodfisher@126.com>
2019-10-29 14:57:34 +0800

    chore:添加自动更新token中间件

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -1,1 +1,48 @@
 <?php
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class RefreshToken extends BaseMiddleware
+{
+    /**
+     * @param         $request
+     * @param Closure $next
+     *
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|mixed
+     * @throws JWTException
+     */
+    public function handle($request, Closure $next)
+    {
+        //检查此次请求中是否带有 token，如果没有则抛出异常。
+        $this->checkForToken($request);
+
+        //使用 try 包裹，以捕捉token过期所抛出的TokenExpiredException异常
+        try {
+            //检测用户的登录状态，如果正常则通过
+            if ($this->auth->parseToken()->authenticate()) {
+                return $next($request);
+            }
+
+            throw new UnauthorizedHttpException('jwt-auth', 'Unauthorized');
+        } catch (TokenExpiredException $e) {
+            try {
+                //刷新token
+                $token = $this->auth->refresh(true, true);
+                //使用一次性登录以保证此次请求的成功
+                Auth::guard()->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
+            } catch (JWTException $e) {
+                //如果捕获到此异常，即代表 refresh 也过期了，用户无法刷新令牌，需要重新登录。
+                throw new UnauthorizedHttpException('jwt-auth', $e->getMessage(), $e, $e->getCode());
+            }
+        }
+
+        // 在响应头中返回新的 token
+        return $this->setAuthenticationHeader($next($request), $token);
+    }
+}
