6192506a5e2876b50ecabbe88790141006af359c
githubfisher <gooodfisher@126.com>
2019-10-29 14:43:22 +0800

    chore:添加管理员及运营的表定义

---
+++ database/migrations/2019_10_29_064045_create_ops_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('pid')->default(0);
+            $table->string('name', 64);
+            $table->char('mobile', 11)->unique();
+            $table->timestamp('mobile_verified_at')->nullable();
+            $table->string('email', 32)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 64)->default('');
+            $table->string('openid', 32)->default('');
+            $table->string('nickname', 64)->default('');
+            $table->string('headimgurl', 255)->default('');
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ops');
+    }
+}
