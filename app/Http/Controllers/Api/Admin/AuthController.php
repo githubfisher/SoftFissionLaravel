713f12a55e56266027c4a40870eb3d3654a34e52
githubfisher <gooodfisher@126.com>
2019-10-29 14:43:58 +0800

    chore:添加管理员及运营的认证控制器

--- app/Http/Controllers/Api/Admin/AuthController.php
+++ app/Http/Controllers/Api/Admin/AuthController.php
@@ -1,1 +1,63 @@
 <?php
+namespace App\Http\Controllers\Api\Admin;
+
+use Auth;
+use Hash;
+use App\Models\Admin\Admin;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'mobile'           => 'mobile',
+            'password'         => 'required|min:6|max:20',
+        ]);
+
+        $user = Admin::create([
+            'mobile'   => $request->get('mobile'),
+            'email'    => $request->get('email'),
+            'username' => $request->get('username'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+        $token = Auth::login($user);
+
+        return response()->json(compact('token'));
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'mobile'   => 'required|mobile|exists:admin',
+            'password' => 'required|string|min:6|max:20',
+        ]);
+
+        $credentials = $request->only(['mobile', 'password']);
+
+        return (($token = Auth::attempt($credentials))
+            ? response()->json(['token' => $token], 201)
+            : response()->json(['error' => '账号或密码错误'], 401));
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(Auth::user());
+    }
+}
