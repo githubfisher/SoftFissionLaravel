b47b24076d41fd82cfa315635273e2009ca930e6
githubfisher <gooodfisher@126.com>
2019-10-29 14:33:52 +0800

    chore:添加用户模型的表名及字段处理定义

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable implements JWTSubject
     use Notifiable;
 
     protected $table = 'user';
-    
+
     /**
      * The attributes that are mass assignable.
      *
