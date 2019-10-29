7f6fba9dc236c28756172d5d9696da42646570bd
githubfisher <gooodfisher@126.com>
2019-10-29 14:17:02 +0800

    chore:添加用户表字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('pid')->default(0);
             $table->string('name');
-            $table->string('email')->unique();
+            $table->char('mobile', 11)->unique();
+            $table->timestamp('mobile_verified_at')->nullable();
+            $table->string('email', 32)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 64)->default('');
+            $table->string('openid', 32)->default('');
+            $table->string('nickname', 64)->default('');
+            $table->string('headimgurl', 255)->default('');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
