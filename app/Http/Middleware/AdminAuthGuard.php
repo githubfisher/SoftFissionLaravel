9870e4d6bc932ef4511885c1cbcfd85a124f0a85
githubfisher <gooodfisher@126.com>
2019-10-29 14:57:17 +0800

    chore:添加管理员,运营人员认证guard切换中间件

--- app/Http/Middleware/AdminAuthGuard.php
+++ app/Http/Middleware/AdminAuthGuard.php
@@ -1,1 +1,22 @@
 <?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminAuthGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        config(['auth.defaults.guard' => 'admin']);
+
+        return $next($request);
+    }
+}
