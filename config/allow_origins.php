56ec5c4bcb8fb1eaa7b731a61328b3ad50a21adb
githubfisher <gooodfisher@126.com>
2019-10-29 15:31:47 +0800

    chore:添加可跨域访问的站点域名

--- config/allow_origins.php
+++ config/allow_origins.php
@@ -1,1 +1,7 @@
 <?php
+return [
+    env('ALLOW_ORIGIN_LOCALHOST', 'http://sf'),
+    env('ALLOW_ORIGIN_DEVELOPMENT', 'http://dev'),
+    env('ALLOW_ORIGIN_TEST', 'http://test'),
+    env('ALLOW_ORIGIN_PRODUCE', 'https://produce'),
+];
