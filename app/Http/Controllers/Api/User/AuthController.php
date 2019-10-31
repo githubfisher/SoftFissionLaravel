12df9b0a36264c88278f4aededadf3c5d0257146
githubfisher <gooodfisher@126.com>
2019-10-31 09:47:51 +0800

    chore:暂时去除api注释

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -11,21 +11,10 @@ use Dingo\Blueprint\Annotation\Method\Get;
 use App\Http\Requests\User\RegisterRequest;
 use Dingo\Blueprint\Annotation\Method\Post;
 
-/**
- * 用户认证
- *
- * @Resource("User\Auth", uri="/user/auth")
- */
 class AuthController extends Controller
 {
     /**
      * 用户注册
-     * @Post("/register")
-     * @Versions({"v1"})
-     * @Transaction({
-     *  @request({"mobile": "13312348765", "password": "123456", "username": "foo", "email": "123@456.com"}),
-     *  @Response(200, body={"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zZlwvdXNlclwvbWUiLCJpYXQiOjE1NzE5NzI5NTksImV4cCI6MTU3MTk3MzA4OCwibmJmIjoxNTcxOTczMDI4LCJqdGkiOiJwY2R2cktDbzg5VzFCSXJGIiwic3ViIjoxfQ.29Zsj7M7Eng6H27K7Hg_lwtgxqdk__EA03r0rg3ythQ"}),
-     * })
      *
      * @param RegisterRequest $request
      *
@@ -46,13 +35,6 @@ class AuthController extends Controller
 
     /**
      * 用户登录
-     * @Post("/login")
-     * @Versions({"v1"})
-     * @Transaction({
-     *  @request({"mobile": "13312348765", "password": "123456"}),
-     *  @Response(201, body={"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zZlwvdXNlclwvbWUiLCJpYXQiOjE1NzE5NzI5NTksImV4cCI6MTU3MTk3MzA4OCwibmJmIjoxNTcxOTczMDI4LCJqdGkiOiJwY2R2cktDbzg5VzFCSXJGIiwic3ViIjoxfQ.29Zsj7M7Eng6H27K7Hg_lwtgxqdk__EA03r0rg3ythQ"}),
-     *  @Response(401, body={"message": "账号或密码错误"})
-     * })
      *
      * @param LoginRequest $request
      *
@@ -60,7 +42,6 @@ class AuthController extends Controller
      */
     public function login(LoginRequest $request)
     {
-
         $credentials = $request->only(['mobile', 'password']);
 
         return (($token = Auth::attempt($credentials))
@@ -70,13 +51,6 @@ class AuthController extends Controller
 
     /**
      * 用户信息
-     * @Get("/me")
-     * @Versions({"v1"})
-     * @Transaction({
-     *  @request(headers={"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zZlwvdXNlclwvbWUiLCJpYXQiOjE1NzE5NzI5NTksImV4cCI6MTU3MTk3MzA4OCwibmJmIjoxNTcxOTczMDI4LCJqdGkiOiJwY2R2cktDbzg5VzFCSXJGIiwic3ViIjoxfQ.29Zsj7M7Eng6H27K7Hg_lwtgxqdk__EA03r0rg3ythQ"}),
-     *  @Response(200, body={"id":1,"pid":0,"username":"foo","mobile":"13312348765","email":"123@456.com","openid":"","nickname":"","headimgurl":"","deleted_at":null,"created_at":"2019-10-24 16:45:09","updated_at":"2019-10-24 16:45:09"}),
-     *  @Response(401, body={"message": "Unauthorized"})
-     * })
      *
      * @return \Illuminate\Http\JsonResponse
      */
@@ -87,14 +61,6 @@ class AuthController extends Controller
 
     /**
      * 用户登出
-     * @Get("/logout")
-     * @Versions({"v1"})
-     * @Transaction({
-     *  @request(headers={"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9zZlwvdXNlclwvbWUiLCJpYXQiOjE1NzE5NzI5NTksImV4cCI6MTU3MTk3MzA4OCwibmJmIjoxNTcxOTczMDI4LCJqdGkiOiJwY2R2cktDbzg5VzFCSXJGIiwic3ViIjoxfQ.29Zsj7M7Eng6H27K7Hg_lwtgxqdk__EA03r0rg3ythQ"}),
-     *  @Response(200, body={}),
-     *  @Response(401, body={"message": "Unauthorized"}),
-     *  @Response(401, body={"message": "Token has expired"}),
-     * })
      *
      * @return \Illuminate\Http\JsonResponse
      */
