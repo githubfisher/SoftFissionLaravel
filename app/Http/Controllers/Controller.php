719e39f543f2a76454469d1285c08ed949750fc4
githubfisher <gooodfisher@126.com>
2019-10-31 22:27:55 +0800

    chore:添加返回response的通用方法

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,32 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
+
+    /**
+     * 返回成功的响应
+     *
+     * @param array $data
+     * @param int $status
+     * @param array $headers
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function suc($data = [], $status = 200, $headers = [])
+    {
+        return response()->json(array_merge(['code' => 0, 'message' => 'success'], $data), $status, $headers);
+    }
+
+    /**
+     * 返回错误的响应
+     *
+     * @param array $data
+     * @param int $status
+     * @param array $headers
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function err($data = [], $status = 400, $headers = [])
+    {
+        return response()->json(array_merge(['code' => 0, 'message' => 'error'], $data), $status, $headers);
+    }
 }
