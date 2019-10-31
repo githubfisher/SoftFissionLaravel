133de4609eb49839f28d29a5f24d436fbe2fbb22
githubfisher <gooodfisher@126.com>
2019-10-31 22:26:12 +0800

    chore:添加常用常量定义

--- app/Http/Utilities/Constant.php
+++ app/Http/Utilities/Constant.php
@@ -1,1 +1,14 @@
 <?php
+namespace App\Http\Utilities;
+
+class Constant
+{
+    const CACHE_TTL_MINUTE      = 60;
+    const CACHE_TTL_TWO_MINUTE  = 120;
+    const CACHE_TTL_FIVE_MINUTE = 300;
+
+    const BASE64_PREFIX_PNG = 'data:image/png;base64,';
+
+    // 缓存短信码 // mobile
+    public const AUTH_SMS_CODE  = 'sf:auth:sms_code:%s';
+}
