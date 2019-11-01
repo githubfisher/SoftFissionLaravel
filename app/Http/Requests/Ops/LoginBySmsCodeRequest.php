1935e1a47449c82722d27652fbaedc06c036eabb
githubfisher <gooodfisher@126.com>
2019-11-01 16:45:15 +0800

    chore:添加手机验证码登录的校验规则

---
+++ app/Http/Requests/Ops/LoginBySmsCodeRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Ops;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginBySmsCodeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'mobile'   => 'required|mobile|exists:ops',
+            'code'     => 'required|string|min:4|max:4',
+        ];
+    }
+
+    /**
+     * 获取验证错误消息。
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'code.required'   => '验证码必须填写',
+            'mobile.required' => '手机号必须填写',
+            'mobile.exists'   => '手机号不存在',
+        ];
+    }
+}
