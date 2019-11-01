4a714b2c9ded3dbb604dfe8ae3814e0a1a0f094d
githubfisher <gooodfisher@126.com>
2019-11-01 14:25:28 +0800

    chore:单独为各个路由分组限流

--- routes/api.php
+++ routes/api.php
@@ -18,60 +18,58 @@ $api->version('v1', [
         'cors',
         'api.throttle',
     ],
-    'expires' => 1,
-    'limit'   => 60,
 ], function (\Dingo\Api\Routing\Router $api) {
 
     /**
      * 无需认证的接口
      */
-    $api->group(['prefix' => '/user/auth'], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['prefix' => '/user/auth', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'User\AuthController@login');
         $api->post('register', 'User\AuthController@register');
     });
 
-    $api->group(['prefix' => '/admin/auth', 'middleware' => 'admin'], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['prefix' => '/admin/auth', 'middleware' => 'admin', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'Admin\AuthController@login');
         $api->post('register', 'Admin\AuthController@register');
     });
 
-    $api->group(['prefix' => '/ops/auth', 'middleware' => 'ops'], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['prefix' => '/ops/auth', 'middleware' => 'ops', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'Ops\AuthController@login');
         $api->post('register', 'Ops\AuthController@register');
     });
 
     $api->group(['prefix' => '/auth'], function (\Dingo\Api\Routing\Router $api) {
         $api->get('captcha', 'Auth\CaptchaController@getCode');
-        $api->get('sms-code', 'Auth\SmsCodeController@getCode');
+        $api->get('sms-code', ['uses' => 'Auth\SmsCodeController@getCode', 'expires' => 1, 'limit' => 1]);
     });
 
     /**
      * 需认证但不需要刷新token的接口
      */
-    $api->group(['middleware' => 'api.auth'], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => 'api.auth', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/user/auth/logout', 'User\AuthController@logout');
     });
 
-    $api->group(['middleware' => ['admin', 'api.auth']], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => ['admin', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/admin/auth/logout', 'Admin\AuthController@logout');
     });
 
-    $api->group(['middleware' => ['ops', 'api.auth']], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => ['ops', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/ops/auth/logout', 'Ops\AuthController@logout');
     });
 
     /**
      * 需认证且刷新token的接口
      */
-    $api->group(['middleware' => ['refresh', 'api.auth']], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => ['refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/user/auth/me', 'User\AuthController@me');
     });
 
-    $api->group(['middleware' => ['admin', 'refresh', 'api.auth']], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => ['admin', 'refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/admin/auth/me', 'Admin\AuthController@me');
     });
 
-    $api->group(['middleware' => ['ops', 'refresh', 'api.auth']], function (\Dingo\Api\Routing\Router $api) {
+    $api->group(['middleware' => ['ops', 'refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/ops/auth/me', 'Ops\AuthController@me');
     });
 });
