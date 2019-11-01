cada2bf7e5fc785fd07704cb017115e96b590254
githubfisher <gooodfisher@126.com>
2019-11-01 15:59:18 +0800

    chore:添加短信场景值的常量定义

--- app/Http/Utilities/Constant.php
+++ app/Http/Utilities/Constant.php
@@ -10,5 +10,8 @@ class Constant
     // Base64图片头
     const BASE64_PREFIX_PNG = 'data:image/png;base64,';
     // 缓存短信码 // scene:mobile
-    const AUTH_SMS_SEND = 'sf:auth:sms_code:%s:%s';
+    const AUTH_SMS_SEND           = 'sf:auth:sms_code:%s:%s';
+    // 短信验证码场景值
+    const SMS_CODE_SCENE_REGISTER = 'register';
+    const SMS_CODE_SCENE_LOGIN    = 'login';
 }
