dd47358c6fad57aa235727653cfa793225662f33
githubfisher <gooodfisher@126.com>
2019-11-01 15:35:00 +0800

    chore:去除短信验证码的部分

--- app/Http/Requests/User/LoginRequest.php
+++ app/Http/Requests/User/LoginRequest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -26,8 +25,7 @@ class LoginRequest extends FormRequest
         return [
             'mobile'   => 'sometimes|required|mobile|exists:user',
             'email'    => 'sometimes|required|email|exists:user',
-            'code'     => 'required_without_all:email,password|string|min:4|max:4',
-            'password' => 'required_without:code|string|min:6|max:20',
+            'password' => 'required|string|min:6|max:20',
         ];
     }
 
@@ -39,7 +37,6 @@ class LoginRequest extends FormRequest
     public function messages()
     {
         return [
-            'code.required'   => '验证码必须填写',
             'mobile.required' => '手机号必须填写',
             'mobile.exists'   => '手机号不存在',
             'email.exists'    => '邮箱不存在',
