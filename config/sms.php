a2e8f55d3f8293c100ab8ab41168689a5f578083
githubfisher <gooodfisher@126.com>
2019-11-01 15:53:45 +0800

    chore:添加短信的配置信息

---
+++ config/sms.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'scene' => [
+        // 注册
+        'register' => [
+            'template_id' => 224794,
+            'cache_ttl'   => 60,
+        ],
+        // 登录
+        'login' => [
+            'template_id' => 224794,
+            'cache_ttl'   => 60,
+        ],
+        // 更新手机号
+        'change' => [
+            'template_id' => 224794,
+            'cache_ttl'   => 60,
+        ],
+        // 身份验证
+        'auth' => [
+            'template_id' => 224794,
+            'cache_ttl'   => 60,
+        ],
+        // 绑定
+        'bind' => [
+            'template_id' => 224794,
+            'cache_ttl'   => 60,
+        ],
+    ],
+];
