e87d6c349c4e50acb529ce0c47ac7182378467b0
githubfisher <gooodfisher@126.com>
2019-11-04 16:50:32 +0800

    chore:添加手机号隐藏部分号码的方法

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * 随机数字验证码
+ */
 if ( ! function_exists('randCode')) {
     function randCode($length = 4)
     {
@@ -11,5 +14,17 @@ if ( ! function_exists('randCode')) {
         }
 
         return $str;
+    }
+}
+
+/* 隐藏手机号中间几位
+ * @param string $mobile 手机号码
+ * @param int $num 屏蔽长度
+ * @return
+ * */
+if ( ! function_exists('hidedMobile')) {
+    function hidedMobile($mobile)
+    {
+        return substr($mobile, 0, 3) . '****' . substr($mobile, -4);
     }
 }
