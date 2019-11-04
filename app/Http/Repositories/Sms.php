a00f9a851c603369a005d160c282c21c40d2dd64
githubfisher <gooodfisher@126.com>
2019-11-04 09:51:31 +0800

    chore:使用配置文件

--- app/Http/Repositories/Sms.php
+++ app/Http/Repositories/Sms.php
@@ -12,32 +12,7 @@ class Sms
 
     public function __construct()
     {
-        $config = [
-            // HTTP 请求的超时时间（秒）
-            'timeout' => 5.0,
-            // 默认发送配置
-            'default' => [
-                // 网关调用策略，默认：顺序调用
-                'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
-                // 默认可用的发送网关
-                'gateways' => [
-                    'qcloud',
-                ],
-            ],
-            // 可用的网关配置
-            'gateways' => [
-                'errorlog' => [
-                    'file' => '/tmp/easy-sms.log',
-                ],
-                'qcloud' => [
-                    'sdk_app_id'  => '1400048706',
-                    'app_key'     => 'cfa16bee4b941d8e1bfbaac98eeaaa49',
-                    'sign_name'   => '小黄人科技',
-                ],
-            ],
-        ];
-
-        $this->sms = new EasySms($config);
+        $this->sms = new EasySms(config('easy_sms'));
     }
 
     public function send($mobile, $params)
