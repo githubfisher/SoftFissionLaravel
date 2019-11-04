9e5b78d05cd661d889556451d803c32c7acb6c82
githubfisher <gooodfisher@126.com>
2019-11-04 15:01:15 +0800

    chore:添加修改失败,密码错误的提示

--- app/Http/Utilities/FeedBack.php
+++ app/Http/Utilities/FeedBack.php
@@ -3,8 +3,13 @@ namespace App\Http\Utilities;
 
 class FeedBack
 {
-    const CAPTCHA_INCORRECT  = ['code' => 1000, 'message' => 'captcha incorrect!'];
-    const SMS_CODE_HAS_SENT  = ['code' => 1001, 'message' => 'sms code has sent!'];
-    const SMS_CODE_SEND_FAIL = ['code' => 1002, 'message' => 'sms code send fail!'];
-    const SMS_CODE_INCORRECT = ['code' => 1003, 'message' => 'sms code incorrect!'];
+    const CAPTCHA_INCORRECT   = ['code' => 1000, 'message' => 'captcha incorrect!'];
+    const SMS_CODE_HAS_SENT   = ['code' => 1001, 'message' => 'sms code has sent!'];
+    const SMS_CODE_SEND_FAIL  = ['code' => 1002, 'message' => 'sms code send fail!'];
+    const SMS_CODE_INCORRECT  = ['code' => 1003, 'message' => 'sms code incorrect!'];
+    const PASSWORD_INCORRECT  = ['code' => 1004, 'message' => 'password incorrect!'];
+    const PASSWORD_RESET_FAIL = ['code' => 1005, 'message' => 'password reset fail!'];
+    const USERNAME_RESET_FAIL = ['code' => 1006, 'message' => 'name reset fail!'];
+    const MOBILE_RESET_FAIL   = ['code' => 1007, 'message' => 'mobile reset fail!'];
+    const SAME_MOBILE         = ['code' => 1008, 'message' => 'same mobile!'];
 }
