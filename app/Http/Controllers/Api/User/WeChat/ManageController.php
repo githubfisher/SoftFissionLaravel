550ee2fad96dba962b407a4d10bd232f1bf1f61c
githubfisher <gooodfisher@126.com>
2019-11-04 16:52:40 +0800

    chore:添加公众号日常操作类

--- app/Http/Controllers/Api/User/WeChat/ManageController.php
+++ app/Http/Controllers/Api/User/WeChat/ManageController.php
@@ -1,1 +1,21 @@
 <?php
+namespace App\Http\Controllers\Api\User\WeChat;
+use App\Http\Controllers\Controller;
+
+class ManageController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function switch()
+    {
+
+    }
+
+    public function unbind()
+    {
+
+    }
+}
