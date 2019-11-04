5af333f4d940e28676c343b24ab5821c0d07d25c
githubfisher <gooodfisher@126.com>
2019-11-04 11:49:22 +0800

    chore:添加权限,角色管理的仓库类

--- app/Http/Repositories/Permission/Permission.php
+++ app/Http/Repositories/Permission/Permission.php
@@ -1,1 +1,17 @@
 <?php
+namespace App\Http\Repositories\Permission;
+
+use \Spatie\Permission\Models\Permission as Permissions;
+
+class Permission
+{
+    public function create(string $name, string $guard)
+    {
+        return Permissions::create(['name' => $name, 'guard_name' => $guard]);
+    }
+
+    public function list(int $limit, string $guard)
+    {
+        return Permissions::where('guard_name', $guard)->simplePaginate($limit);
+    }
+}
--- app/Http/Repositories/Permission/Role.php
+++ app/Http/Repositories/Permission/Role.php
@@ -1,1 +1,17 @@
 <?php
+namespace App\Http\Repositories\Permission;
+
+use \Spatie\Permission\Models\Role as Roles;
+
+class Role
+{
+    public function create(string $name, string $guard)
+    {
+        return Roles::create(['name' => $name, 'guard_name' => $guard]);
+    }
+
+    public function list(int $limit, string $guard)
+    {
+        return Roles::where('guard_name', $guard)->simplePaginate($limit);
+    }
+}
