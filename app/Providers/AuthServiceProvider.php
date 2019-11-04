e0a1793a0f68bac870fac7093345b5d76d079f0f
githubfisher <gooodfisher@126.com>
2019-11-04 11:49:58 +0800

    chore:清理无用的引用类

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
