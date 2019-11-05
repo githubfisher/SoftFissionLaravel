6fb27685e829f41c9c11d19402b55afe6320b246
githubfisher <gooodfisher@126.com>
2019-11-05 17:24:25 +0800

    chore:添加站内信查询的实现方法

--- app/Http/Repositories/Message/Mail.php
+++ app/Http/Repositories/Message/Mail.php
@@ -1,1 +1,48 @@
 <?php
+namespace App\Http\Repositories\Message;
+
+use App\Http\Utilities\Constant;
+use Illuminate\Support\Facades\Redis;
+use App\Models\User\Message\Mail as Message;
+
+class Mail
+{
+    // 查询未读消息数量
+    public function unread(int $userId)
+    {
+        $key = sprintf(Constant::MAIL_UNREAD, $userId);
+        if (Redis::exists($key)) {
+            $unread = Redis::get($key);
+        } else {
+            $unread = Message::Local($userId)->Unread()->count();
+        }
+
+        return $unread;
+    }
+
+    // 消息列表
+    public function list(int $userId, int $limit = Constant::PAGINATE_MIN)
+    {
+        return Message::Local($userId)->paginate($limit);
+    }
+
+    // 设置消息已读
+    public function setRead(int $userId, array $ids = [])
+    {
+        $query = Message::Local($userId)->Unread();
+        if (count($ids)) {
+            $query = $query->whereIn('id', $ids);
+        }
+        $res = $query->update(['status' => Constant::TRUE_ONE]);
+        if ($res) {
+            $key = sprintf(Constant::MAIL_UNREAD, $userId);
+            Redis::del($key);
+            if (count($ids)) {
+                Redis::set($key, $this->unread($userId));
+            } else {
+                Redis::set($key, Constant::FLASE_ZERO);
+            }
+            Redis::expire($key, Constant::CACHE_TTL_TEN_MINUTE);
+        }
+    }
+}
