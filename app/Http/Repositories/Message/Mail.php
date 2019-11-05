544a2306df3b19a56e82e7f324242f5ad7445d2c
githubfisher <gooodfisher@126.com>
2019-11-05 17:34:08 +0800

    chore:添加生成站内信的实现方法

--- app/Http/Repositories/Message/Mail.php
+++ app/Http/Repositories/Message/Mail.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Repositories\Message;
 
+use Carbon\Carbon;
 use App\Http\Utilities\Constant;
 use Illuminate\Support\Facades\Redis;
 use App\Models\User\Message\Mail as Message;
@@ -44,5 +45,92 @@ class Mail
             }
             Redis::expire($key, Constant::CACHE_TTL_TEN_MINUTE);
         }
+    }
+
+    /**
+     * 添加消息到消息中心
+     * @param $params
+     * @return boolean
+     * @author fisher
+     * @date 2018/11/1 下午7:53
+     */
+    public static function addMessage(...$params)
+    {
+        $msg = self::mergeMsg($params);
+        if ( ! empty($msg)) {
+            if ($id = Message::insertGetId($msg)) {
+                return $id;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 组合消息
+     * @param $params
+     * @return array
+     */
+    public static function mergeMsg($params): array
+    {
+        if (isset($params[0]) && isset($params[1])) {
+            $now = Carbon::now()->toDateTimeString();
+
+            $template = Constant::MAIL_TEMPLATE[$params[1]];
+            $data     = [
+                'user_id'    => $params[0],
+                'scene_code' => $params[1],
+                'title'      => $template[0],
+                'content'    => $template[1],
+                'status'     => 0,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+
+            $count = count($params);
+            if ($count > 2) {
+                switch ($count) {
+                    case 3:
+                        $data['content'] = sprintf($data['content'], $params[2]);
+
+                        break;
+                    case 4:
+                        $data['content'] = sprintf($data['content'], $params[2], $params[3]);
+
+                        break;
+                    case 5:
+                        $data['content'] = sprintf($data['content'], $params[2], $params[3], $params[4]);
+
+                        break;
+                    case 6:
+                        $data['content'] = sprintf($data['content'], $params[2], $params[3], $params[4], $params[5]);
+
+                        break;
+                }
+            }
+
+            return $data;
+        }
+
+        return [];
+    }
+
+    /**
+     * 批量添加
+     * @param array ...$params
+     */
+    public static function addAll(...$params)
+    {
+        $userIds = array_shift($params);
+        $data    = [];
+        foreach ($userIds as $userId) {
+            array_unshift($params, $userId);
+            $msg = self::mergeMsg(array_values($params));
+            if ( ! empty($msg)) {
+                $data[] = $msg;
+            }
+        }
+
+        (new Message())->addAll($data);
     }
 }
