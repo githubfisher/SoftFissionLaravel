11cf6ead3f0de696c46e10a91ecfe5723e8d9d26
githubfisher <gooodfisher@126.com>
2019-11-05 12:44:04 +0800

    chore:添加公众号列表,切换,解绑的方法

--- app/Http/Controllers/Api/User/WeChat/ManageController.php
+++ app/Http/Controllers/Api/User/WeChat/ManageController.php
@@ -2,19 +2,29 @@
 namespace App\Http\Controllers\Api\User\WeChat;
 
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\WeChatApp\App;
+use App\Http\Requests\User\WeChat\SwitchRequest;
 
 class ManageController extends Controller
 {
-    public function index()
+    public function index(App $apps)
     {
-        
+        $list = $apps->list($this->user()->id);
+
+        return $this->suc(compact('list'));
     }
 
-    public function switch()
+    public function switch(SwitchRequest $request, App $apps)
     {
+        $res = $apps->switchApp($this->user()->id, $request->input('app_id'));
+
+        return $res ? $this->suc() : $this->err($res);
     }
 
-    public function unbind()
+    public function unbind(SwitchRequest $request, App $apps)
     {
+        $res = $apps->unbind($this->user()->id, $request->input('app_id'));
+
+        return $res ? $this->suc() : $this->err($res);
     }
 }
