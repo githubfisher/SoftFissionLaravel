81bcd9ecd7c63cce6702651cf1f23670a424f0b1
githubfisher <gooodfisher@126.com>
2019-11-05 15:46:48 +0800

    chore:添加注释,简化各方法的职能,加入缓存层

--- app/Http/Repositories/WeChatApp/App.php
+++ app/Http/Repositories/WeChatApp/App.php
@@ -8,75 +8,123 @@ use Illuminate\Support\Facades\Redis;
 
 class App
 {
-    public function addUnbindSet($appId)
+    // 数据库标记公众号解绑
+    public function delApp(string $appId, int $userId = 0)
+    {
+        $condition                                = ['app_id' => $appId];
+        ! empty($userId) && $condition['user_id'] = $userId;
+        WeChatApp::where($condition)->delete();
+    }
+
+    // 添加到解绑集合
+    public function addUnbindSet(string $appId)
     {
         Redis::zAdd(Constant::UNBINDED_APP_ZSET, time(), $appId);
-        WeChatApp::where('app_id', $appId)->delete();
     }
 
-    public function remUnbindSet($appId)
+    // 不校验是否在集合, 直接执行清除操作
+    public function remUnbindSet(string $appId)
     {
-        $rank = Redis::zRank(Constant::UNBINDED_APP_ZSET, $appId);
-        if (is_numeric($rank)) {
-            Redis::zRem(Constant::UNBINDED_APP_ZSET, $appId);
+        Redis::zRem(Constant::UNBINDED_APP_ZSET, $appId);
+    }
+
+    // 仅返回绑定中的公众号
+    public function first(string $appId)
+    {
+        return WeChatApp::where(['app_id' => $appId])->first();
+    }
+
+    // 创建或更新公众号信息
+    public function updateOrCreate(string $appId, array $appInfo)
+    {
+        return WeChatApp::withTrashed()->updateOrCreate(['app_id' => $appId], $appInfo);
+    }
+
+    // 刷新用户名下公众号列表
+    public function refreshAppList(int $userId, array $appInfo)
+    {
+        $list            = $this->list($userId);
+        $list['current'] = $appInfo['app_id'];
+        if ( ! in_array($appInfo['app_id'], array_keys($list['list']))) {
+            $List['list'][$appInfo['app_id']] = $appInfo;
         }
-    }
-
-    public function first($condition)
-    {
-        return WeChatApp::where($condition)->first();
-    }
-
-    public function updateOrCreate($appId, $appData)
-    {
-        return WeChatApp::withTrashed()->updateOrCreate(['app_id' => $appId], $appData);
-    }
-
-    public function refreshAppList($userId, $currentAppId)
-    {
-        $list = WeChatApp::where('user_id', $userId)->get();
-        $list = $list ? $list->toArray() : [];
-        $list = [
-            'currentId' => $currentAppId,
-            'list'      => $list,
-        ];
         $key = sprintf(Constant::BIND_APP_LIST, $userId);
         Redis::set($key, json_encode($list));
 
         return $list;
     }
 
-    public function refreshAppInfo($appId)
+    // 刷新公众号缓存信息
+    public function refreshAppInfo(array $appInfo)
     {
-        //更新单个APP信息
+        $key = sprintf(Constant::BIND_APP_INFO, $appInfo['app_id']);
+        Redis::set($key, json_encode($appInfo));
+    }
+
+    // 用户名下公众号列表
+    public function list(int $userId)
+    {
+        $key  = sprintf(Constant::BIND_APP_LIST, $userId);
+        $list = Redis::get($key);
+        $list = json_decode($list, true);
+        if ($list === false || ! isset($list['current']) || ! isset($list['list']) || empty($list['current']) || empty($list['list'])) {
+            $list = [
+                'current' => '',
+                'list'    => [
+
+                ],
+            ];
+            $appList = WeChatApp::where('user_id', $userId)->get();
+            if ($appList) {
+                $appList         = $appList->toArray();
+                $list['list']    = array_column($appList, null, 'app_id');
+                $list['current'] = $appList[0]['app_id'];
+            }
+
+            Redis::set($key, json_encode($list));
+        }
+
+        return $list;
+    }
+
+    // 公众号信息
+    public function getAppInfo(string $appId)
+    {
         $key     = sprintf(Constant::BIND_APP_INFO, $appId);
-        $appInfo = WeChatApp::withTrashed()->where('app_id', $appId)->first();
+        $appInfo = Redis::get($key);
+        $appInfo = json_decode($appInfo, true);
+        if ($appInfo === false) {
+            $appInfo = $this->first($appId);
+            if ($appInfo) {
+                Redis::set($key, json_encode($appInfo));
+            }
+        }
+
+        return $appInfo;
+    }
+
+    // 切换公众号
+    public function switchApp(int $userId, string $appId)
+    {
+        $appInfo = $this->getAppInfo($appId);
         if ($appInfo) {
-            $appInfo = $appInfo->toArray();
-            Redis::set($key, json_encode($appInfo));
-        }
-    }
+            $list   = $this->refreshAppList($userId, $appInfo);
+            $appIds = array_column($list['list'], 'app_id');
+            if ( ! in_array($appId, $appIds)) {
+                return FeedBack::SWITCH_FAIL;
+            }
 
-    public function list($userId)
-    {
-        return WeChatApp::where('user_id', $userId)->get();
-    }
-
-    public function switchApp($userId, $appId)
-    {
-        $list   = $this->refreshAppList($userId, $appId);
-        $appIds = array_column($list['list'], 'app_id');
-        if ( ! in_array($appId, $appIds)) {
-            return FeedBack::SWITCH_FAIL;
+            return true;
         }
 
-        return true;
+        return false;
     }
 
-    public function unbind($userId, $appId)
+    // 解绑公众号
+    public function unbind(string $appId, int $userId = 0)
     {
-        WeChatApp::where(['user_id' => $userId, 'app_id' => $appId])->delete();
         $this->addUnbindSet($appId);
+        $this->delApp($appId, $userId);
 
         return true;
     }
