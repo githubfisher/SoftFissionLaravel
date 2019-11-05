ed51b6e8fab1f03997b50535abde0fadffe6229d
githubfisher <gooodfisher@126.com>
2019-11-05 12:25:32 +0800

    style:整理格式

--- app/Http/Controllers/Api/User/WeChat/ManageController.php
+++ app/Http/Controllers/Api/User/WeChat/ManageController.php
@@ -1,21 +1,20 @@
 <?php
 namespace App\Http\Controllers\Api\User\WeChat;
+
 use App\Http\Controllers\Controller;
 
 class ManageController extends Controller
 {
     public function index()
     {
-
+        
     }
 
     public function switch()
     {
-
     }
 
     public function unbind()
     {
-
     }
 }
