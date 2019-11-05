8adc887290f94788e6a545169080b650a013fa89
githubfisher <gooodfisher@126.com>
2019-11-05 18:10:22 +0800

    doc:添加注释

--- routes/api.php
+++ routes/api.php
@@ -22,29 +22,34 @@ $api->version('v1', [
     /**
      * 无需认证的接口
      */
+    // 用户认证
     $api->group(['prefix' => '/user/auth', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'User\AuthController@login');
         $api->post('register', 'User\AuthController@register');
         $api->post('login-by-sms-code', 'User\AuthController@loginBySmsCode');
     });
 
+    // 管理员认证
     $api->group(['prefix' => '/admin/auth', 'middleware' => 'admin', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'Admin\AuthController@login');
         $api->post('register', 'Admin\AuthController@register');
         $api->post('login-by-sms-code', 'Admin\AuthController@loginBySmsCode');
     });
 
+    // 运营认证
     $api->group(['prefix' => '/ops/auth', 'middleware' => 'ops', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->post('login', 'Ops\AuthController@login');
         $api->post('register', 'Ops\AuthController@register');
         $api->post('login-by-sms-code', 'Ops\AuthController@loginBySmsCode');
     });
 
+    // 公有-认证
     $api->group(['prefix' => '/auth'], function (\Dingo\Api\Routing\Router $api) {
         $api->get('captcha', 'Auth\CaptchaController@getCode');
         $api->get('sms-code', ['uses' => 'Auth\SmsCodeController@getCode', 'expires' => 1, 'limit' => 1]);
     });
 
+    // 公众号授权
     $api->group(['prefix' => 'wechat'], function (\Dingo\Api\Routing\Router $api) {
         $api->post('serve', 'User\WeChat\WeChatController@serve');
         $api->post('bind/callback', 'User\WeChat\WeChatController@bindCallBack');
@@ -53,22 +58,16 @@ $api->version('v1', [
     /**
      * 需认证但不需要刷新token的接口
      */
-    $api->group(['middleware' => 'api.auth', 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
-        $api->get('/user/auth/logout', 'User\AuthController@logout');
-    });
-
-    $api->group(['middleware' => ['admin', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
-        $api->get('/admin/auth/logout', 'Admin\AuthController@logout');
-    });
-
-    $api->group(['middleware' => ['ops', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
-        $api->get('/ops/auth/logout', 'Ops\AuthController@logout');
-    });
+    $api->get('/user/auth/logout', ['middleware' => 'api.auth', 'expires' => 1, 'limit' => 60, 'uses' => 'User\AuthController@logout']);
+    $api->get('/admin/auth/logout', ['middleware' => ['admin', 'api.auth'], 'expires' => 1, 'limit' => 60, 'uses' => 'Admin\AuthController@logout']);
+    $api->get('/ops/auth/logout', ['middleware' => ['ops', 'api.auth'], 'expires' => 1, 'limit' => 60, 'uses' => 'Ops\AuthController@logout']);
 
     /**
      * 需认证且刷新token的接口
      */
+    // 用户端
     $api->group(['middleware' => ['refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
+        // 个人中心
         $api->group(['prefix' => '/user/auth'], function (\Dingo\Api\Routing\Router $api) {
             $api->get('me', 'User\AuthController@me');
             $api->put('rename', 'User\UserController@resetName');
@@ -76,6 +75,7 @@ $api->version('v1', [
             $api->put('remobile', 'User\UserController@resetMobile');
             $api->put('repwd', 'User\UserController@resetPassword');
         });
+        // 权限
         $api->group(['prefix' => '/role'], function (\Dingo\Api\Routing\Router $api) {
             $api->get('', 'Permission\RoleController@index');
             $api->post('create', 'Permission\RoleController@create');
@@ -84,12 +84,14 @@ $api->version('v1', [
             $api->get('', 'Permission\PermissionController@index');
             $api->post('create', 'Permission\PermissionController@create');
         });
+        // 公众号管理
         $api->get('/wechat/binding', 'User\WeChat\WeChatController@binding');
         $api->group(['prefix' => '/wechat/apps'], function (\Dingo\Api\Routing\Router $api) {
             $api->get('', 'User\WeChat\ManageController@index');
             $api->get('switch', 'User\WeChat\ManageController@switchApp');
             $api->get('unbind', 'User\WeChat\ManageController@unbind');
         });
+        // 站内信
         $api->group(['prefix' => '/mail'], function (\Dingo\Api\Routing\Router $api) {
             $api->get('', 'User\Message\MailController@index');
             $api->get('unread', 'User\Message\MailController@unread');
@@ -97,10 +99,12 @@ $api->version('v1', [
         });
     });
 
+    // 管理后台
     $api->group(['middleware' => ['admin', 'refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/admin/auth/me', 'Admin\AuthController@me');
     });
 
+    // 运营后台
     $api->group(['middleware' => ['ops', 'refresh', 'api.auth'], 'expires' => 1, 'limit' => 60], function (\Dingo\Api\Routing\Router $api) {
         $api->get('/ops/auth/me', 'Ops\AuthController@me');
     });
