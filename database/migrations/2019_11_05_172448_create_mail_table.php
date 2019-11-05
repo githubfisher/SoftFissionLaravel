39713fde898479fb35e99ad1906cc13758890942
githubfisher <gooodfisher@126.com>
2019-11-05 17:28:35 +0800

    chore:添加站内信表的结构

---
+++ database/migrations/2019_11_05_172448_create_mail_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id')->default(0);
+            $table->unsignedInteger('scene_code')->default(0);
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail');
+    }
+}
