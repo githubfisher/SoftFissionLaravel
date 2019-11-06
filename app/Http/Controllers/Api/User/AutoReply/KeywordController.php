4b9d0d489694ff5ce58fbbd7fc43728250f53054
githubfisher <gooodfisher@126.com>
2019-11-06 10:49:56 +0800

    chore:添加关键词回复的控制器

---
+++ app/Http/Controllers/Api/User/AutoReply/KeywordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\User\AutoReply;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KeywordController extends Controller
+{
+    //
+}
