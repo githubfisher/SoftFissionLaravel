907f964592af099fb9839bfcf0d28307cc57816e
githubfisher <gooodfisher@126.com>
2019-11-06 17:47:50 +0800

    chore:创建失败时返回失败信息

--- app/Http/Repositories/Reply/Keyword.php
+++ app/Http/Repositories/Reply/Keyword.php
@@ -4,6 +4,7 @@ namespace App\Http\Repositories\Reply;
 use DB;
 use Log;
 use App\Http\Utilities\Constant;
+use App\Http\Utilities\FeedBack;
 use App\Models\User\Reply\Rules;
 use App\Models\User\Reply\Replies;
 use App\Models\User\Reply\Keywords;
@@ -22,7 +23,6 @@ class Keyword
 
     public function store($params)
     {
-        $ruleId   = false;
         // {keyword:, match_type:}
         $keywords = $params['keywords'];
         // {difference:, reply_type:, reply_type_female:, content:, content_female:, material_id:, material_id_female}
@@ -47,16 +47,47 @@ class Keyword
             } catch (\Exception $e) {
                 Log::error(__FUNCTION__ . ' ' . $e->getMessage() . "\n" . $e->getTraceAsString());
                 DB::rollBack();
+
+                return FeedBack::CREATE_FAIL;
+            }
+
+            DB::commit();
+
+            return $ruleId;
+        }
+
+        return FeedBack::PARAMS_INCORRECT;
+    }
+
+    public function update($id, $params)
+    {
+        // {keyword:, match_type:}
+        $keywords = $params['keywords'];
+        // {difference:, reply_type:, reply_type_female:, content:, content_female:, material_id:, material_id_female}
+        $replies  = $params['replies'];
+        if (is_array($keywords) && is_array($replies) && count($keywords) && count($replies)) {
+            DB::beginTransaction();
+
+            try {
+                $ruleId = Rules::where('id', $id)->update([
+                    'title'      => $params['title'],
+                    'reply_rule' => $params['reply_rule'],
+                    'start_at'   => $params['start_at'],
+                    'end_at'     => $params['end_at'],
+                ]);
+
+                data_fill($keywords, '*.rule_id', $ruleId);
+                data_fill($replies, '*.rule_id', $ruleId);
+                (new Keywords)->addAll($keywords);
+                (new Replies())->addAll($replies);
+            } catch (\Exception $e) {
+                Log::error(__FUNCTION__ . ' ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+                DB::rollBack();
             }
 
             DB::commit();
         }
 
-        return $ruleId;
-    }
-
-    public function update($id, $params)
-    {
-        
+        return true;
     }
 }
