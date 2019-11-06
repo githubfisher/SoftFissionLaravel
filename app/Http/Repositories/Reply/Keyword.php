53758cebe90035c83498d13001c4111e772cb411
githubfisher <gooodfisher@126.com>
2019-11-06 17:31:08 +0800

    chore:添加创建关键词回复的实现

--- app/Http/Repositories/Reply/Keyword.php
+++ app/Http/Repositories/Reply/Keyword.php
@@ -1,13 +1,62 @@
 <?php
 namespace App\Http\Repositories\Reply;
 
+use DB;
+use Log;
 use App\Http\Utilities\Constant;
 use App\Models\User\Reply\Rules;
+use App\Models\User\Reply\Replies;
+use App\Models\User\Reply\Keywords;
 
 class Keyword
 {
     public function list($userId, $appId, $scene, $limit = Constant::PAGINATE_MIN)
     {
         return Rules::Local($userId)->App($appId)->Scene($scene)->Recent()->paginate($limit);
+    }
+
+    public function get($id)
+    {
+        return Rules::with(['keywords', 'replies'])->find($id);
+    }
+
+    public function store($params)
+    {
+        $ruleId   = false;
+        // {keyword:, match_type:}
+        $keywords = $params['keywords'];
+        // {difference:, reply_type:, reply_type_female:, content:, content_female:, material_id:, material_id_female}
+        $replies  = $params['replies'];
+        if (is_array($keywords) && is_array($replies) && count($keywords) && count($replies)) {
+            DB::beginTransaction();
+
+            try {
+                $ruleId = Rules::insertGetId([
+                    'user_id'    => $params['user_id'],
+                    'app_id'     => $params['app_id'],
+                    'title'      => $params['title'],
+                    'reply_rule' => $params['reply_rule'],
+                    'start_at'   => $params['start_at'],
+                    'end_at'     => $params['end_at'],
+                ]);
+
+                data_fill($keywords, '*.rule_id', $ruleId);
+                data_fill($replies, '*.rule_id', $ruleId);
+                (new Keywords)->addAll($keywords);
+                (new Replies())->addAll($replies);
+            } catch (\Exception $e) {
+                Log::error(__FUNCTION__ . ' ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+                DB::rollBack();
+            }
+
+            DB::commit();
+        }
+
+        return $ruleId;
+    }
+
+    public function update($id, $params)
+    {
+        
     }
 }
