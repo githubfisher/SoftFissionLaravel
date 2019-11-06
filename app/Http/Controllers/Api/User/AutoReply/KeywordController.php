b9e4a24323ee6e76ba297a36ba6190e85ac05be9
githubfisher <gooodfisher@126.com>
2019-11-06 16:20:43 +0800

    chore:添加关键词回复的列表方法

--- app/Http/Controllers/Api/User/AutoReply/KeywordController.php
+++ app/Http/Controllers/Api/User/AutoReply/KeywordController.php
@@ -1,38 +1,35 @@
 <?php
-
 namespace App\Http\Controllers\Api\User\AutoReply;
 
-use App\Http\Controllers\Controller;
-use App\Http\Repositories\AutoReply\Keyword;
-use App\Http\Requests\User\AutoReply\KeywordRequest;
 use Illuminate\Http\Request;
+use App\Http\Utilities\Constant;
+use App\Http\Controllers\Controller;
+use App\Http\Repositories\Reply\Keyword;
+use App\Http\Requests\User\AutoReply\KeywordRequest;
 
 class KeywordController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param KeywordRequest $request
      * @param Keyword $keyword
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Keyword $keyword)
+    public function index(KeywordRequest $request, Keyword $keyword)
     {
-        $list = $keyword->list();
+        $list = $keyword->list($this->user()->id, $request->input('app_id'), Constant::REPLY_RULE_SCENE_KEYWORD);
 
         return $this->suc(compact('list'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @param KeywordRequest $request
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function create(KeywordRequest $request)
+    public function create()
     {
-        return $this->suc();
+        //
     }
 
     /**
@@ -54,16 +51,17 @@ class KeywordController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
+    public function show($id, Keyword $keyword)
     {
-        return $this->suc([$id]);
+        $data = $keyword->get($id);
+
+        return $this->suc(compact('data'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
@@ -73,23 +71,35 @@ class KeywordController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param         $id
+     * @param Keyword $keyword
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, Keyword $keyword)
     {
-        //
+        if ($keyword->update($id, $request->only(['']))) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param         $id
+     * @param Keyword $keyword
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
+    public function destroy($id, Keyword $keyword)
     {
-        //
+        if ($keyword->destroy($id)) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 }
