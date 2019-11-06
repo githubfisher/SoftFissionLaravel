bf2287cac84a41492d8f21459b16a308ec478698
githubfisher <gooodfisher@126.com>
2019-11-06 10:09:29 +0800

    chore:添加微信消息处理的伪代码方法

--- app/Http/Controllers/Api/User/WeChat/WeChatController.php
+++ app/Http/Controllers/Api/User/WeChat/WeChatController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers\Api\User\WeChat;
 
 use Log;
 use EasyWeChat\Factory;
+use Illuminate\Http\Request;
+use App\Http\Utilities\Constant;
 use App\Http\Utilities\FeedBack;
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\WeChatApp\App;
@@ -117,5 +119,132 @@ EOF;
         Log::error(__FUNCTION__ . ' ' . $appId . ', 更新或创建公众号信息失败! userId: ' . $userId);
 
         return redirect($frontDomain . '/#/bind/fail?message=' . FeedBack::BIND_FAIL['message']);
+    }
+
+    public function message(Request $request, App $apps)
+    {
+        $appId = $request->input('appId');
+
+        try {
+            if (empty($appId)) {
+                throw new \Exception('appId is empty!', 999);
+            }
+
+            // 判断公众号是否已解绑(1灵猴后台解绑, 2微信公众后台解绑)
+            if ( ! $apps->isBinded($appId)) {
+                return $this->success();
+            }
+
+            $appInfo = $apps->getAppInfo($appId);
+            if (empty($appInfo)) {
+                $apps->unbind($appId);
+
+                return $this->success();
+            }
+
+            $message = $this->openPlatform->server->getMessage();
+            // 微信认证的公众号获取粉丝信息
+            $openId          = $message['FromUserName'];
+            $officialAccount = $this->openPlatform->officialAccount($appInfo['app_id'], $appInfo['refresh_token']);
+            $fansInfo        = [
+                'openid'     => '',
+                'nickname'   => '',
+                'headimgurl' => '',
+            ];
+            if ($appInfo['verify_type_info'] >= Constant::FLASE_ZERO && ! empty($openId)) {
+                $fans = $officialAccount->user->get($openId);
+                if (isset($fans['errcode']) && ! empty($fans['errcode'])) {
+                    Log::debug(__FUNCTION__ . ' 公众号未认证, 未能获取粉丝信息; 或获取粉丝信息失败. appid: ' . $appInfo['app_id'] . ' ' . json_encode($fans));
+                } else {
+                    $fansInfo = $fans;
+                }
+            }
+
+            $server = $officialAccount->server;
+            $server->push(function ($message) use ($officialAccount, $fansInfo, $appInfo) {
+                $msgType = $message['MsgType'];
+                $message['fansInfo'] = $fansInfo;
+                $message['appInfo']  = $appInfo;
+
+                switch ($msgType) {
+                    case 'event':
+                        $this->logMsg($message, Constant::FLASE_ZERO);
+
+                        break;
+                    case 'text':
+                        $this->logMsg($message, Constant::FLASE_ZERO);
+
+                        break;
+                    default:
+                        $this->logMsg($message, Constant::FLASE_ZERO);
+
+                        break;
+                }
+
+                return 'SUCCESS';
+            });
+
+            $response = $server->serve();
+
+            return response($response->getContent())->send();
+        } catch (\Throwable $throwable) {
+            $message = $throwable->getMessage();
+            if (strpos($message, ':61023,') !== false) {
+                $apps->unbind($appId);
+                Log::debug(__FUNCTION__ . ' app: ' . $appId . ', refresh token invalid, 加入到已解绑公众号集合! ' . $throwable->getCode());
+            } elseif (strpos($message, 'Invalid Signature') !== false || strpos($message, 'Invalid appId') !== false) {
+                Log::error(__FUNCTION__ . ' ' . $message);
+            } elseif (Constant::TRUE_ONE != $throwable->getCode()) {
+                $data = [
+                    'code'    => $throwable->getCode(),
+                    'title'   => '微信消息处理异常',
+                    'appid'   => $appId,
+                    'message' => $message,
+                    'file'    => $throwable->getFile(),
+                    'line'    => $throwable->getLine(),
+                    'trace'   => $throwable->getTrace(),
+                ];
+                Log::error(__FUNCTION__ . ' ' . json_encode($data));
+            }
+        }
+
+        return $this->success();
+    }
+
+    /**
+     * 返回给微信服务器, 成功接收并处理消息
+     *
+     * @author fisher
+     * @date 2019-03-18 09:53
+     */
+    private function success()
+    {
+        return response('SUCCESS')->send();
+    }
+
+    // 记录消息信息入日志
+    private function logMsg($message, $msgType = Constant::MSG_TYPE_OTHER)
+    {
+        $endfix = chr(27) . '[0m';
+        // 昵称(openid)->APPID(公众号昵称)
+        $nickname = isset($message['fansInfo']['nickname']) ? $message['fansInfo']['nickname'] : '';
+        $str      = chr(27) . '[36m' . $nickname . $endfix . '(' . $message['FromUserName'] . ')->' . $message['appInfo']['app_id'] . '(' . chr(27) . '[35m' . $message['appInfo']['nick_name'] . $endfix . ')';
+
+        // 事件或文本信息
+        switch ($msgType) {
+            case Constant::MSG_TYPE_EVENT:
+                $subStr = isset($message['EventKey']) ? '(' . $message['EventKey'] . ') ' : '() ';
+                $str    = $message['Event'] . $endfix . $subStr . $str;
+
+                break;
+            case Constant::MSG_TYPE_TEXT:
+                $str = $message['Content'] . $endfix . ' ' . $str;
+
+                break;
+            default:
+                $str = $message['MsgType'] . $endfix . ' ' . $str;
+        }
+
+        Log::debug(chr(27) . '[32m' . $str);
     }
 }
