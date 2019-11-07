d3f85aad306b2659a5bca0b4d32991853fdd349d
githubfisher <gooodfisher@126.com>
2019-11-07 14:51:01 +0800

    chore:简化规则策略,仅判断是否有权限,不判断所属

--- app/Policies/User/Reply/RulePolicy.php
+++ app/Policies/User/Reply/RulePolicy.php
@@ -2,7 +2,6 @@
 namespace App\Policies\User\Reply;
 
 use App\Models\User\User;
-use App\Models\User\Reply\Rule;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class RulePolicy
@@ -10,31 +9,23 @@ class RulePolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can view the post.
      *
      * @param  \App\Models\User\User $user
-     * @param  \App\Models\User\Reply\Rule $rule
      * @return mixed
      */
-    public function view(User $user, Rule $rule)
+    public function view(User $user)
     {
         // visitors cannot view unpublished items
         if ($user === null) {
             return false;
         }
 
-        // authors can view their own unpublished posts
-        return $user->id == $rule->user_id;
+        if ($user->can('view rules')) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -45,6 +36,11 @@ class RulePolicy
      */
     public function create(User $user)
     {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
         if ($user->can('create rules')) {
             return true;
         }
@@ -56,13 +52,17 @@ class RulePolicy
      * Determine whether the user can update the post.
      *
      * @param  \App\Models\User\User $user
-     * @param  \App\Models\User\Reply\Rule $rule
      * @return mixed
      */
-    public function update(User $user, Rule $rule)
+    public function update(User $user)
     {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
         if ($user->can('edit own rules')) {
-            return $user->id == $rule->user_id;
+            return true;
         }
 
         return false;
@@ -72,17 +72,17 @@ class RulePolicy
      * Determine whether the user can delete the post.
      *
      * @param  \App\Models\User\User $user
-     * @param  \App\Models\User\Reply\Rule $rule
      * @return mixed
      */
-    public function delete(User $user, Rule $rule)
+    public function delete(User $user)
     {
-        if ($user->can('delete own rules')) {
-            if ($user->pid == 0) {
-                return $user->id == $rule->user_id;
-            }
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
 
-            return $user->pid == $rule->user_id;
+        if ($user->can('delete own rules')) {
+            return true;
         }
 
         return false;
