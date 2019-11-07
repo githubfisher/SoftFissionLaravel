5e324bbf377dcebb58287a6f9ea9802024db363a
githubfisher <gooodfisher@126.com>
2019-11-07 15:17:08 +0800

    chore:添加角色查询,分配,移除的控制器方法

--- app/Http/Controllers/Api/Permission/RoleController.php
+++ app/Http/Controllers/Api/Permission/RoleController.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Http\Controllers\Api\Permission;
 
+use App\Models\User\User;
 use Illuminate\Http\Request;
 use App\Http\Utilities\Constant;
 use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -28,5 +30,36 @@ class RoleController extends Controller
         $role = $this->role->create($request->input('name'), $this->guard);
 
         return $this->suc(compact('role'));
+    }
+
+    public function assignRole($role, $user)
+    {
+        $role = Role::findOrFail($role);
+        $user = User::findOrFail($user);
+
+        if ($res = $this->role->assignRole($role, $user)) {
+            return $this->suc();
+        }
+
+        return $this->err();
+    }
+
+    public function removeRole($role, $user)
+    {
+        $role = Role::findOrFail($role);
+        $user = User::findOrFail($user);
+
+        if ($res = $this->role->removeRole($role, $user)) {
+            return $this->suc();
+        }
+
+        return $this->err();
+    }
+
+    public function allMyRoles()
+    {
+        $list = $this->role->getAllRoles($this->user());
+
+        return $this->suc(compact('list'));
     }
 }
