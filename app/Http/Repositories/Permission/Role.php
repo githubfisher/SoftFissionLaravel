272d64b7a1e970b149c5ae59071ee09e03a2ff41
githubfisher <gooodfisher@126.com>
2019-11-07 15:16:17 +0800

    chore:添加分配,移除,查询角色的实现方法

--- app/Http/Repositories/Permission/Role.php
+++ app/Http/Repositories/Permission/Role.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Repositories\Permission;
 
+use App\Models\User\User;
 use \Spatie\Permission\Models\Role as Roles;
 
 class Role
@@ -13,5 +14,20 @@ class Role
     public function list(int $limit, string $guard)
     {
         return Roles::where('guard_name', $guard)->simplePaginate($limit);
+    }
+
+    public function assignRole(Roles $role, User $user)
+    {
+        return $user->assignRole($role);
+    }
+
+    public function removeRole(Roles $role, User $user)
+    {
+        return $user->removeRole($role);
+    }
+
+    public function getAllRoles(User $user)
+    {
+        return $user->getRoleNames();
     }
 }
