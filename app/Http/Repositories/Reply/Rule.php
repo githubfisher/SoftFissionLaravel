62a1b72bb6af494ee1c87761a19ba57dd6bcdc5a
githubfisher <gooodfisher@126.com>
2019-11-07 16:03:11 +0800

    chore:修正删除的条件

--- app/Http/Repositories/Reply/Rule.php
+++ app/Http/Repositories/Reply/Rule.php
@@ -138,6 +138,6 @@ class Rule
 
     public function destroy($userId, $appId, $id)
     {
-        return Rules::Local($userId)->App($appId)->delete($id);
+        return Rules::Local($userId)->App($appId)->where('id', $id)->delete();
     }
 }
