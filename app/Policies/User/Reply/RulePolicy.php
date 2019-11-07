bd2230bf724ced5fc44e4953e2fad18afd32e7f0
githubfisher <gooodfisher@126.com>
2019-11-07 10:59:20 +0800

    chore:添加首个策略,回复规则策略

---
+++ app/Policies/User/Reply/RulePolicy.php
@@ -0,0 +1,85 @@
+<?php
+namespace App\Policies\User\Reply;
+
+use App\Models\User\User;
+use App\Models\User\Reply\Rule;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RulePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\Models\User\User $user
+     * @param  \App\Models\User\Reply\Rule $rule
+     * @return mixed
+     */
+    public function view(User $user, Rule $rule)
+    {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
+        // authors can view their own unpublished posts
+        return $user->id == $rule->user_id;
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\Models\User\User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if ($user->can('create rules')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\Models\User\User $user
+     * @param  \App\Models\User\Reply\Rule $rule
+     * @return mixed
+     */
+    public function update(User $user, Rule $rule)
+    {
+        if ($user->can('edit own rules')) {
+            return $user->id == $rule->user_id;
+        }
+
+        return false;
+    }
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\Models\User\User $user
+     * @param  \App\Models\User\Reply\Rule $rule
+     * @return mixed
+     */
+    public function delete(User $user, Rule $rule)
+    {
+        if ($user->can('delete own rules')) {
+            return $user->id == $rule->user_id;
+        }
+
+        return false;
+    }
+}
