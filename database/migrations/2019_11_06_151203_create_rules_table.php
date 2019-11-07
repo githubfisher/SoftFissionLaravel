3153149e597329a8e5a4ba8a6a614880127ce59f
githubfisher <gooodfisher@126.com>
2019-11-07 11:42:19 +0800

    chore:合并回复规则3张表的migration

--- database/migrations/2019_11_06_151203_create_rules_table.php
+++ database/migrations/2019_11_06_151203_create_rules_table.php
@@ -26,6 +26,37 @@ class CreateRulesTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('rule_id')->default(0);
+            $table->string('keyword', 64);
+            $table->unsignedTinyInteger('match_type')->default(1)->comment('匹配规则: 1全匹配 2半匹配');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('rule_id')
+                  ->references('id')->on('rules')
+                  ->onDelete('cascade');
+        });
+
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('rule_id')->default(0);
+            $table->unsignedTinyInteger('difference')->comment('是否区分男女: 1不区分 2区分');
+            $table->unsignedTinyInteger('reply_type')->comment('回复消息类型: 1文本 2图文 3图片 4音频 5视频 6位置 ...');
+            $table->unsignedTinyInteger('reply_type_female');
+            $table->text('content');
+            $table->text('content_female');
+            $table->unsignedInteger('material_id')->comment('素材ID');
+            $table->unsignedInteger('material_id_female');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('rule_id')
+                  ->references('id')->on('rules')
+                  ->onDelete('cascade');
+        });
     }
 
     /**
@@ -36,5 +67,7 @@ class CreateRulesTable extends Migration
     public function down()
     {
         Schema::dropIfExists('rules');
+        Schema::dropIfExists('keywords');
+        Schema::dropIfExists('replies');
     }
 }
