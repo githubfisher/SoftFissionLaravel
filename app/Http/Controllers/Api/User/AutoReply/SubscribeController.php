821791457cb0615edbc6501e6e48a6d88884f4c5
githubfisher <gooodfisher@126.com>
2019-11-07 17:51:52 +0800

    chore:添加关注回复的控制器

--- app/Http/Controllers/Api/User/AutoReply/SubscribeController.php
+++ app/Http/Controllers/Api/User/AutoReply/SubscribeController.php
@@ -1,1 +1,43 @@
 <?php
+namespace App\Http\Controllers\Api\User\AutoReply;
+
+use App\Http\Utilities\Constant;
+use App\Http\Controllers\Controller;
+use App\Http\Repositories\Reply\Rule;
+use App\Models\User\Reply\Rule as Rules;
+use App\Http\Requests\User\AutoReply\RuleRequest;
+use App\Http\Requests\User\AutoReply\CreateRuleRequest;
+
+class SubscribeController extends Controller
+{
+    protected $rule;
+
+    public function __construct(Rule $rule)
+    {
+        $this->rule = $rule;
+    }
+
+    public function store(CreateRuleRequest $request)
+    {
+        $this->authorize('create', Rules::class);
+
+        $params            = $request->all();
+        $params['user_id'] = $this->user()->id;
+        $params['scene']   = Constant::REPLY_RULE_SCENE_SUBSCRIBE;
+        $res               = $this->rule->storeSubscribeRule($params);
+        if (is_numeric($res)) {
+            return $this->suc(['id' => $res]);
+        }
+
+        return $this->err($res);
+    }
+
+    public function show(RuleRequest $request)
+    {
+        $this->authorize('view', Rules::class);
+
+        $data = $this->rule->getSubscribeRule($this->user()->id, $request->input('app_id'));
+
+        return $this->suc(compact('data'));
+    }
+}
