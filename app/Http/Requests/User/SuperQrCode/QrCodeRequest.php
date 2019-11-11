1d053735caa49a6e8867eb745640fbf91305d296
githubfisher <gooodfisher@126.com>
2019-11-11 16:00:49 +0800

    chore:添加二维码请求的校验规则类

---
+++ app/Http/Requests/User/SuperQrCode/QrCodeRequest.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Requests\User\SuperQrCode;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class QrCodeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'app_id' => 'required|string|min:18',
+            'limit'  => 'sometimes|required|integer|min:10',
+        ];
+    }
+}
