6f298621940f4be255e367049dc05ea0fa3a044b
githubfisher <gooodfisher@126.com>
2019-11-11 16:13:03 +0800

    chore:完善二维码模型的定义

--- app/Models/User/SuperQrCode/QrCode.php
+++ app/Models/User/SuperQrCode/QrCode.php
@@ -1,10 +1,33 @@
 <?php
-
 namespace App\Models\User\SuperQrCode;
 
 use Illuminate\Database\Eloquent\Model;
 
 class QrCode extends Model
 {
-    //
+    protected $table    = 'super_qr_code';
+    protected $fillable = [
+        'user_id',
+        'app_id',
+        'rule_id',
+        'title',
+        'target_num',
+        'num',
+        'expire_type',
+        'expire_at',
+        'expire_in',
+        'status',
+    ];
+    protected $hidden  = [];
+    protected $guarded = ['id'];
+
+    public function details()
+    {
+        return $this->hasMany('App\Models\User\SuperQrCode\QrCodeDetail', 'qrcode_id', 'id');
+    }
+
+    public function rule()
+    {
+        return $this->hasOne('App\Models\User\Reply\Rule', 'id', 'rule_id');
+    }
 }
--- app/Models/User/SuperQrCode/QrCodeDetail.php
+++ app/Models/User/SuperQrCode/QrCodeDetail.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Models\User\SuperQrCode;
 
 use Illuminate\Database\Eloquent\Model;
 
 class QrCodeDetail extends Model
 {
-    //
+    protected $table    = 'super_qr_code_detail';
+    protected $fillable = [
+        'qrcode_id',
+        'batch',
+        'scene_str',
+        'scan_num',
+        'subscribe_num',
+        'url',
+        'ticket',
+        'expire_at',
+    ];
+    protected $hidden  = [];
+    protected $guarded = ['id'];
+
+    public function code()
+    {
+        return $this->belongsTo('App\Models\User\SuperQrCode\QrCode', 'qrcode_id');
+    }
 }
