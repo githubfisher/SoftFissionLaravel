798ec8a0de798f96fec130b483a6ff44b2d41f62
githubfisher <gooodfisher@126.com>
2019-11-11 16:25:58 +0800

    chore:添加二维码的访问权限规则

--- app/Policies/User/SuperQrCode/QrCodePolicy.php
+++ app/Policies/User/SuperQrCode/QrCodePolicy.php
@@ -10,12 +10,82 @@ class QrCodePolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the post.
      *
-     * @return void
+     * @param  \App\Models\User\User $user
+     * @return mixed
      */
-    public function __construct()
+    public function view(User $user)
     {
-        //
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
+        if ($user->can('view qrcode')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\Models\User\User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
+        if ($user->can('create qrcode')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\Models\User\User $user
+     * @return mixed
+     */
+    public function update(User $user)
+    {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
+        if ($user->can('edit own qrcode')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\Models\User\User $user
+     * @return mixed
+     */
+    public function delete(User $user)
+    {
+        // visitors cannot view unpublished items
+        if ($user === null) {
+            return false;
+        }
+
+        if ($user->can('delete own qrcode')) {
+            return true;
+        }
+
+        return false;
     }
 }
