992635e9ca968d1187d03ad3fe9b1bdf3cccd3b3
githubfisher <gooodfisher@126.com>
2019-11-11 17:50:06 +0800

    chore:填充图片素材控制器类的各方法

--- app/Http/Controllers/Api/User/Material/ImageController.php
+++ app/Http/Controllers/Api/User/Material/ImageController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Api\User\Material;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\Material\Image;
 
 /**
  * 图片素材
@@ -11,9 +12,27 @@ use App\Http\Controllers\Controller;
  */
 class ImageController extends Controller
 {
-    public function index()
+    protected $images;
+
+    public function __construct(Image $images)
     {
-        //
+        $this->images = $images;
+    }
+
+    /**
+     * @param NewsRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function index(NewsRequest $request)
+    {
+        $this->authorize('view', Material::class);
+
+        $limit = $request->input('limit', Constant::PAGINATE_MIN);
+        $list  = $this->news->list($this->user()->id, $request->input('app_id'), Constant::REPLY_RULE_SCENE_KEYWORD, $limit);
+
+        return $this->suc(compact('list'));
     }
 
     public function create()
@@ -21,14 +40,39 @@ class ImageController extends Controller
         //
     }
 
-    public function store(Request $request)
+    /**
+     * @param CreateNewsRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(CreateNewsRequest $request)
     {
-        //
+        $this->authorize('create', Material::class);
+
+        $params            = $request->all();
+        $params['user_id'] = $this->user()->id;
+        $res               = $this->news->store($params);
+        if (is_numeric($res)) {
+            return $this->suc(['id' => $res]);
+        }
+
+        return $this->err($res);
     }
 
+    /**
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function show($id)
     {
-        //
+        $this->authorize('view', Material::class);
+
+        $data = $this->news->get($id);
+
+        return $this->suc(compact('data'));
     }
 
     public function edit($id)
@@ -36,13 +80,39 @@ class ImageController extends Controller
         //
     }
 
-    public function update(Request $request, $id)
+    /**
+     * @param CreateNewsRequest $request
+     * @param                   $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(CreateNewsRequest $request, $id)
     {
-        //
+        $this->authorize('update', Material::class);
+
+        if ($this->news->update($id, $request->all())) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 
-    public function destroy($id)
+    /**
+     * @param NewsRequest $request
+     * @param             $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(NewsRequest $request, $id)
     {
-        //
+        $this->authorize('delete', Material::class);
+
+        if ($this->news->destroy($this->user()->id, $request->input('app_id'), $id)) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 }
