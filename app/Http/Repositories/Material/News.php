bd6eb6aa4b5c0bc3285f55d9ac69062f74f34437
githubfisher <gooodfisher@126.com>
2019-11-11 17:59:43 +0800

    chore:添加图文列表,详情的实现方法

--- app/Http/Repositories/Material/News.php
+++ app/Http/Repositories/Material/News.php
@@ -1,6 +1,29 @@
 <?php
 namespace App\Http\Repositories\Material;
 
+use App\Models\User\Material\News as Material;
+
 class News
 {
+    public function list($userId, $appId, $limit)
+    {
+        return Material::Local($userId)->App($appId)->Recent()->simplePaginate($limit);
+    }
+
+    public function get($id, $userId, $appId)
+    {
+        return Material::Local($userId)->App($appId)->findOrFail($id);
+    }
+
+    public function create()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function destory()
+    {
+    }
 }
