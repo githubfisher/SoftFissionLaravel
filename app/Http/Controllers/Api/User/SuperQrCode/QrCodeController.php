e88afcaf181fe82b87531907b06612e6e85c625d
githubfisher <gooodfisher@126.com>
2019-11-11 16:02:04 +0800

    chore:添加二维码控制器类

--- app/Http/Controllers/Api/User/SuperQrCode/QrCodeController.php
+++ app/Http/Controllers/Api/User/SuperQrCode/QrCodeController.php
@@ -1,9 +1,12 @@
 <?php
-
 namespace App\Http\Controllers\Api\User\SuperQrCode;
 
+use App\Http\Utilities\Constant;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User\SuperQrCode\QrCode;
+use App\Http\Requests\User\SuperQrCode\QrCodeRequest;
+use App\Http\Repositories\SuperQrCode\QrCode as QrCodes;
+use App\Http\Requests\User\SuperQrCode\CreateQrCodeRequest;
 
 /**
  * 超级二维码
@@ -12,9 +15,27 @@ use Illuminate\Http\Request;
  */
 class QrCodeController extends Controller
 {
-    public function index()
+    protected $qrcode;
+
+    public function __construct(QrCodes $qrCode)
     {
-        //
+        $this->qrcode = $qrCode;
+    }
+
+    /**
+     * @param QrCodeRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function index(QrCodeRequest $request)
+    {
+        $this->authorize('view', QrCode::class);
+
+        $limit = $request->input('limit', Constant::PAGINATE_MIN);
+        $list  = $this->qrcode->list($this->user()->id, $request->input('app_id'), $limit);
+
+        return $this->suc(compact('list'));
     }
 
     public function create()
@@ -22,14 +43,40 @@ class QrCodeController extends Controller
         //
     }
 
-    public function store(Request $request)
+    /**
+     * @param CreateQrCodeRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(CreateQrCodeRequest $request)
     {
-        //
+        $this->authorize('create', QrCode::class);
+
+        $params            = $request->all();
+        $params['user_id'] = $this->user()->id;
+        $params['scene']   = Constant::REPLY_RULE_SCENE_SCAN;
+        $res               = $this->qrcode->store($params);
+        if (is_numeric($res)) {
+            return $this->suc(['id' => $res]);
+        }
+
+        return $this->err($res);
     }
 
+    /**
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function show($id)
     {
-        //
+        $this->authorize('view', QrCode::class);
+
+        $data = $this->qrcode->get($id);
+
+        return $this->suc(compact('data'));
     }
 
     public function edit($id)
@@ -37,13 +84,41 @@ class QrCodeController extends Controller
         //
     }
 
-    public function update(Request $request, $id)
+    /**
+     * @param CreateQrCodeRequest $request
+     * @param                     $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(CreateQrCodeRequest $request, $id)
     {
-        //
+        $this->authorize('update', QrCode::class);
+
+        $res = $this->qrcode->update($id, $request->all());
+        if ($res === true) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 
-    public function destroy($id)
+    /**
+     * @param QrCodeRequest $request
+     * @param               $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(QrCodeRequest $request, $id)
     {
-        //
+        $this->authorize('delete', QrCode::class);
+
+        $res = $this->qrcode->destroy($this->user()->id, $request->input('app_id'), $id);
+        if ($res === true) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 }
