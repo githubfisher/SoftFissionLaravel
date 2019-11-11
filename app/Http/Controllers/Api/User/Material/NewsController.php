dd32874c42155ad1fb8302788bf972d6787d4df0
githubfisher <gooodfisher@126.com>
2019-11-11 17:49:38 +0800

    chore:填充图文素材控制器类的各方法

--- app/Http/Controllers/Api/User/Material/NewsController.php
+++ app/Http/Controllers/Api/User/Material/NewsController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Http\Controllers\Api\User\Material;
 
-use Illuminate\Http\Request;
+use App\Http\Utilities\Constant;
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\Material\News;
+use App\Models\User\Material\News as Material;
+use App\Http\Requests\User\Material\NewsRequest;
+use App\Http\Requests\User\Material\CreateNewsRequest;
 
 /**
  * 图文素材
@@ -11,9 +15,27 @@ use App\Http\Controllers\Controller;
  */
 class NewsController extends Controller
 {
-    public function index()
+    protected $news;
+
+    public function __construct(News $news)
     {
-        //
+        $this->news = $news;
+    }
+
+    /**
+     * @param NewsRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function index(NewsRequest $request)
+    {
+        $this->authorize('view', Material::class);
+
+        $limit = $request->input('limit', Constant::PAGINATE_MIN);
+        $list  = $this->news->list($this->user()->id, $request->input('app_id'), Constant::REPLY_RULE_SCENE_KEYWORD, $limit);
+
+        return $this->suc(compact('list'));
     }
 
     public function create()
@@ -21,14 +43,39 @@ class NewsController extends Controller
         //
     }
 
-    public function store(Request $request)
+    /**
+     * @param CreateNewsRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(CreateNewsRequest $request)
     {
-        //
+        $this->authorize('create', Material::class);
+
+        $params            = $request->all();
+        $params['user_id'] = $this->user()->id;
+        $res               = $this->news->store($params);
+        if (is_numeric($res)) {
+            return $this->suc(['id' => $res]);
+        }
+
+        return $this->err($res);
     }
 
+    /**
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function show($id)
     {
-        //
+        $this->authorize('view', Material::class);
+
+        $data = $this->news->get($id);
+
+        return $this->suc(compact('data'));
     }
 
     public function edit($id)
@@ -36,13 +83,39 @@ class NewsController extends Controller
         //
     }
 
-    public function update(Request $request, $id)
+    /**
+     * @param CreateNewsRequest $request
+     * @param                   $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(CreateNewsRequest $request, $id)
     {
-        //
+        $this->authorize('update', Material::class);
+
+        if ($this->news->update($id, $request->all())) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 
-    public function destroy($id)
+    /**
+     * @param NewsRequest $request
+     * @param             $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(NewsRequest $request, $id)
     {
-        //
+        $this->authorize('delete', Material::class);
+
+        if ($this->news->destroy($this->user()->id, $request->input('app_id'), $id)) {
+            return $this->suc();
+        }
+
+        return $this->err();
     }
 }
