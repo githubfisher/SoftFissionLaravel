a3743fedbe4808f8e3feb9756c209c16f92b675c
githubfisher <gooodfisher@126.com>
2019-11-11 17:35:32 +0800

    chore:添加微信素材各个控制器类

---
+++ app/Http/Controllers/Api/User/Material/NewsController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers\Api\User\Material;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * 图文素材
+ * Class NewsController
+ * @package App\Http\Controllers\Api\User\Material
+ */
+class NewsController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/User/Material/VideoController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers\Api\User\Material;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * 视频素材
+ * Class VideoController
+ * @package App\Http\Controllers\Api\User\Material
+ */
+class VideoController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
