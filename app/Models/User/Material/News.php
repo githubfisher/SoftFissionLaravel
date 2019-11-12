f450a1d389c27f870d84ae622c728c4526d6d852
githubfisher <gooodfisher@126.com>
2019-11-12 14:42:56 +0800

    chore:完善微信图文模型定义

--- app/Models/User/Material/News.php
+++ app/Models/User/Material/News.php
@@ -5,29 +5,17 @@ use App\Models\Model;
 
 class News extends Model
 {
-    protected $table    = 'news';
+    protected $table    = 'material_news';
     protected $fillable = [
         'user_id',
         'app_id',
-        'rule_id',
-        'title',
-        'target_num',
-        'num',
-        'expire_type',
-        'expire_at',
-        'expire_in',
-        'status',
+        'media_id',
     ];
     protected $hidden  = [];
     protected $guarded = ['id'];
 
     public function details()
     {
-        return $this->hasMany('App\Models\User\SuperQrCode\QrCodeDetail', 'qrcode_id', 'id');
-    }
-
-    public function rule()
-    {
-        return $this->hasOne('App\Models\User\Reply\Rule', 'id', 'rule_id');
+        return $this->hasMany('App\Models\User\Material\NewsDetail', 'news_id', 'id');
     }
 }
--- app/Models/User/Material/NewsDetail.php
+++ app/Models/User/Material/NewsDetail.php
@@ -5,5 +5,25 @@ use App\Models\Model;
 
 class NewsDetail extends Model
 {
-    //
+    protected $table    = 'material_news_detail';
+    protected $fillable = [
+        'news_id',
+        'thumb_media_id',
+        'sort',
+        'show_cover_pic',
+        'title',
+        'author',
+        'digest',
+        'thumb_url',
+        'url',
+        'content_source_url',
+        'content',
+    ];
+    protected $hidden  = [];
+    protected $guarded = ['id'];
+
+    public function news()
+    {
+        return $this->belongsTo('App\Models\User\Material\News', 'news_id');
+    }
 }
