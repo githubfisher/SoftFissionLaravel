fcc07cbb4ef74470d62e01a889fbb0721e18317a
githubfisher <gooodfisher@126.com>
2019-11-12 17:55:16 +0800

    chore:自动回复参数校验规则更新,具体到数组内部

--- app/Http/Requests/User/AutoReply/CreateRuleRequest.php
+++ app/Http/Requests/User/AutoReply/CreateRuleRequest.php
@@ -23,13 +23,22 @@ class CreateRuleRequest extends FormRequest
     public function rules()
     {
         return [
-            'app_id'     => 'required|string|min:18',
-            'title'      => 'sometimes|required|string',
-            'keywords'   => 'sometimes|required|array',
-            'replies'    => 'required|array',
-            'reply_rule' => 'required|in:1,2',
-            'start_at'   => 'nullable|date',
-            'end_at'     => 'nullable|date',
+            'app_id'                       => 'required|string|min:18',
+            'title'                        => 'sometimes|required|string',
+            'keywords'                     => 'sometimes|required|array',
+            'keywords.*.keyword'           => 'required_with:keywords|string|min:1|max:64',
+            'keywords.*.match_type'        => 'required_with:keywords|integer|in:1,2',
+            'replies'                      => 'required|array|min:1',
+            'replies.*.difference'         => 'required|integer|in:1,2',
+            'replies.*.reply_type'         => 'sometimes|nullable|integer|in:1,2,3,4,5,6',
+            'replies.*.reply_type_female'  => 'sometimes|nullable|integer|in:1,2,3,4,5,6',
+            'replies.*.content'            => 'sometimes|nullable|string|max:2048',
+            'replies.*.content_female'     => 'sometimes|nullable|string|max:2048',
+            'replies.*.material_id'        => 'sometimes|nullable|integer|min:1',
+            'replies.*.material_id_female' => 'sometimes|nullable|integer|min:1',
+            'reply_rule'                   => 'required|in:1,2',
+            'start_at'                     => 'nullable|date',
+            'end_at'                       => 'nullable|date',
         ];
     }
 }
