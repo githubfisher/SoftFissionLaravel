9772ecdad74e83a6cf6f99a744a948edc0d79c9c
githubfisher <gooodfisher@126.com>
2019-11-12 17:50:58 +0800

    chore:二维码创建及更新,回复内容规则,校验具体到数组内部

--- app/Http/Requests/User/SuperQrCode/CreateQrCodeRequest.php
+++ app/Http/Requests/User/SuperQrCode/CreateQrCodeRequest.php
@@ -23,17 +23,24 @@ class CreateQrCodeRequest extends FormRequest
     public function rules()
     {
         return [
-            'app_id'      => 'required|string|min:18',
-            'title'       => 'sometimes|required|string',
-            'replies'     => 'required|array|min:1',
-            'reply_rule'  => 'required|in:1,2',
-            'start_at'    => 'nullable|date',
-            'end_at'      => 'nullable|date',
-            'target_num'  => 'sometimes|required|integer|min:1',
-            'type'        => 'required|integer|in:1,2',
-            'expire_type' => 'required_if:type,1|integer|in:1,2',
-            'expire_in'   => 'required_if:expire_type,1|integer|min:1',
-            'expire_at'   => 'required_if:expire_type,2|date',
+            'app_id'                       => 'required|string|min:18',
+            'title'                        => 'sometimes|required|string',
+            'replies'                      => 'required|array|min:1',
+            'replies.*.difference'         => 'required|integer|in:1,2',
+            'replies.*.reply_type'         => 'sometimes|nullable|integer|in:1,2,3,4,5,6',
+            'replies.*.reply_type_female'  => 'sometimes|nullable|integer|in:1,2,3,4,5,6',
+            'replies.*.content'            => 'sometimes|nullable|string|max:2048',
+            'replies.*.content_female'     => 'sometimes|nullable|string|max:2048',
+            'replies.*.material_id'        => 'sometimes|nullable|integer|min:1',
+            'replies.*.material_id_female' => 'sometimes|nullable|integer|min:1',
+            'reply_rule'                   => 'required|in:1,2',
+            'start_at'                     => 'nullable|date',
+            'end_at'                       => 'nullable|date',
+            'target_num'                   => 'sometimes|required|integer|min:1',
+            'type'                         => 'required|integer|in:1,2',
+            'expire_type'                  => 'required_if:type,1|integer|in:1,2',
+            'expire_in'                    => 'required_if:expire_type,1|integer|min:1',
+            'expire_at'                    => 'required_if:expire_type,2|date',
         ];
     }
 }
