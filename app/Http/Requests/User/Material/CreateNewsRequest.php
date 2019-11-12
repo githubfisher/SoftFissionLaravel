ec4e2a341d5376fa8a9bd1e513ae21db3f5842fb
githubfisher <gooodfisher@126.com>
2019-11-12 16:16:07 +0800

    chore:修改图文的链接校验规则

--- app/Http/Requests/User/Material/CreateNewsRequest.php
+++ app/Http/Requests/User/Material/CreateNewsRequest.php
@@ -26,11 +26,11 @@ class CreateNewsRequest extends FormRequest
             'app_id'                       => 'required|string|min:18',
             'details'                      => 'required|array|min:1',
             'details.*.title'              => 'required|string|max:192',
-            'details.*.thumb_url'          => 'required|string|max:255',
+            'details.*.thumb_url'          => 'required|url|max:255',
             'details.*.image_id'           => 'required|integer|max:1',
             'details.*.digest'             => 'sometimes|nullable|string|max:192',
             'details.*.author'             => 'sometimes|nullable|string|max:24',
-            'details.*.content_source_url' => 'sometimes|nullable|string|max:255',
+            'details.*.content_source_url' => 'sometimes|nullable|url|max:255',
             'details.*.content'            => 'sometimes|nullable|string',
             'details.*.poster_id'          => 'sometimes|nullable|integer|min:1',
         ];
