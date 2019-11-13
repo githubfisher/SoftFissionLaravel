5801edb6b742f1018ceaaaf4e37df4d662b15d4e
githubfisher <gooodfisher@126.com>
2019-11-13 10:21:24 +0800

    chore:映射旧公用方法名到辅助方法上

---
+++ app/Repositories/Blog/PostRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Blog;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface PostRepository.
+ *
+ * @package namespace App\Repositories\Blog;
+ */
+interface PostRepository extends RepositoryInterface
+{
+    //
+}
