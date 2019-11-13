03ba6ca01135d18709d7cd944e16404f325ca1d5
githubfisher <gooodfisher@126.com>
2019-11-13 11:47:53 +0800

    chore:迁移Repository扩展的模型定义

--- app/Entities/Material/News.php
+++ app/Entities/Material/News.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Entities\Material;
 
 use Illuminate\Database\Eloquent\Model;
@@ -15,11 +14,17 @@ class News extends Model implements Transformable
 {
     use TransformableTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [];
+    protected $table    = 'material_news';
+    protected $fillable = [
+        'user_id',
+        'app_id',
+        'media_id',
+    ];
+    protected $hidden  = [];
+    protected $guarded = ['id'];
 
+    public function details()
+    {
+        return $this->hasMany('App\Models\User\Material\NewsDetail', 'news_id', 'id');
+    }
 }
