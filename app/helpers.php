6c5ffb061dd2ee957badf3fb9e402cf5576c9026
githubfisher <gooodfisher@126.com>
2019-11-13 11:07:49 +0800

    chore:全路径引入类Str

--- app/helpers.php
+++ app/helpers.php
@@ -28,3 +28,24 @@ if ( ! function_exists('hidedMobile')) {
         return substr($mobile, 0, 3) . '****' . substr($mobile, -4);
     }
 }
+
+if ( ! function_exists('str_plural')) {
+    function str_plural(string $value, int $count = 2)
+    {
+        return \Illuminate\Support\Str::plural($value, $count);
+    }
+}
+
+if ( ! function_exists('snake_case')) {
+    function snake_case(string $value, string $delimiter = '_')
+    {
+        return \Illuminate\Support\Str::snake($value, $delimiter);
+    }
+}
+
+if ( ! function_exists('str_contains')) {
+    function str_contains(string $haystack, $needles)
+    {
+        return \Illuminate\Support\Str::contains($haystack, $needles);
+    }
+}
