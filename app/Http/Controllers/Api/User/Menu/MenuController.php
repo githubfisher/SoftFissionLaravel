2a038cb97f8949d31461a01c182f2fb311126d46
githubfisher <gooodfisher@126.com>
2019-11-13 10:00:18 +0800

    chore:添加自定义菜单的控制器类及操作类

---
+++ app/Http/Controllers/Api/User/Menu/MenuController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Controllers\Api\User\Menu;
+
+use App\Http\Controllers\Controller;
+use App\Http\Repositories\Material\Voice;
+
+class MenuController extends Controller
+{
+    protected $voice;
+
+    public function __construct(Voice $voice)
+    {
+        $this->voice = $voice;
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
---
+++ app/Http/Repositories/Menu/Menu.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Http\Repositories\Menu;
+class Menu
+{
+
+}
