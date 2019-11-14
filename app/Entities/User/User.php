510adc6b57d56504c4cd554b5f624a4328f8ab6f
githubfisher <gooodfisher@126.com>
2019-11-14 17:15:30 +0800

    chore:迁移User模型

---
+++ app/Entities/User/User.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Entities\User;
+
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Class User.
+ *
+ * @package namespace App\Entities\User;
+ */
+class User extends Authenticatable implements JWTSubject, Transformable
+{
+    use TransformableTrait, Notifiable, HasRoles;
+
+    protected $table = 'user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'mobile', 'openid', 'nickname', 'headimgurl',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ app/Repositories/User/UserRepositoryEloquent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\Entities\User\User;
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+
+/**
+ * Class UserRepositoryEloquent.
+ *
+ * @package namespace App\Repositories\User;
+ */
+class UserRepositoryEloquent extends BaseRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+    /**
+     * Specify Validator class name
+     *
+     * @return mixed
+     */
+    public function validator()
+    {
+        return 'App\\Validators\\User\\UserValidator';
+    }
+}
