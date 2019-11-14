1793a44d38e127249e2cf5f02df6301aa7a3bc1d
githubfisher <gooodfisher@126.com>
2019-11-14 18:00:29 +0800

    chore:短信验证码的有效时长,适应env

--- config/sms.php
+++ config/sms.php
@@ -5,27 +5,27 @@ return [
         // 注册
         'register' => [
             'template_id' => 224794,
-            'cache_ttl'   => 60,
+            'cache_ttl'   => env('SMS_CODE_TTL', 60),
         ],
         // 登录
         'login' => [
             'template_id' => 224794,
-            'cache_ttl'   => 60,
+            'cache_ttl'   => env('SMS_CODE_TTL', 60),
         ],
         // 更新手机号
         'reset' => [
             'template_id' => 224794,
-            'cache_ttl'   => 60,
+            'cache_ttl'   => env('SMS_CODE_TTL', 60),
         ],
         // 身份验证
         'auth' => [
             'template_id' => 224794,
-            'cache_ttl'   => 60,
+            'cache_ttl'   => env('SMS_CODE_TTL', 60),
         ],
         // 绑定
         'bind' => [
             'template_id' => 224794,
-            'cache_ttl'   => 60,
+            'cache_ttl'   => env('SMS_CODE_TTL', 60),
         ],
     ],
 ];
