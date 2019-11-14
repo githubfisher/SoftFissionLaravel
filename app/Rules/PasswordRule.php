ac990f1d6e1b6adb4fba2ff1556a292ea1811fee
githubfisher <gooodfisher@126.com>
2019-11-14 16:49:14 +0800

    chore:添加可复用的密码验证规则

---
+++ app/Rules/PasswordRule.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Rules;
+
+use Illuminatech\Validation\Composite\CompositeRule;
+
+class PasswordRule extends CompositeRule
+{
+    protected function rules(): array
+    {
+        return ['string', 'min:6', 'max:20', 'alpha_dash'];
+    }
+
+    public function message()
+    {
+        return '密码格式错误';
+    }
+}
