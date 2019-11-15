b89d8d5c71662668bf5660504a060228b2e15663
githubfisher <gooodfisher@126.com>
2019-11-15 12:47:57 +0800

    chore:迁移公众号模型定义

--- app/Entities/WeChat/App.php
+++ app/Entities/WeChat/App.php
@@ -3,6 +3,7 @@
 namespace App\Entities\WeChat;
 
 use App\Models\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
@@ -13,13 +14,27 @@ use Prettus\Repository\Traits\TransformableTrait;
  */
 class App extends Model implements Transformable
 {
-    use TransformableTrait;
+    use TransformableTrait, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [];
-
+    protected $table    = 'we_app';
+    protected $fillable = [
+        'user_id',
+        'app_id',
+        'nick_name',
+        'head_img',
+        'user_name',
+        'qrcode_url',
+        'refresh_token',
+        'service_type_info',
+        'verify_type_info',
+        'alias',
+        'principal_name',
+        'signature',
+        'keyword_reply',
+        'anytype_reply',
+        'subscribe_reply',
+    ];
+    protected $hidden  = ['alias','principal_name','signature'];
+    protected $guarded = ['id'];
+    protected $dates   = ['deleted_at'];
 }
