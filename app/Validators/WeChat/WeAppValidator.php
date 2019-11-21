ce3d2f41edb4e59835ee669957f76432234654e8
githubfisher <gooodfisher@126.com>
2019-11-21 16:22:33 +0800

    chore:addAppCreateOrUpdateValidatorRule

--- app/Validators/WeChat/WeAppValidator.php
+++ app/Validators/WeChat/WeAppValidator.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Validators\WeChat;
 
-use \Prettus\Validator\Contracts\ValidatorInterface;
 use \Prettus\Validator\LaravelValidator;
+use \Prettus\Validator\Contracts\ValidatorInterface;
 
 /**
  * Class AppValidator.
@@ -18,7 +17,36 @@ class WeAppValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'user_id'            => ['required', 'integer'],
+            'app_id'             => ['required', 'string', 'min:18', 'alpha_dash'],
+            'refresh_token'      => ['required', 'string'],
+            'nick_name'          => ['required', 'string'],
+            'head_img'           => ['required', 'url'],
+            'user_name'          => ['required', 'string'],
+            'alias'              => ['nullable', 'string'],
+            'qrcode_url'         => ['required', 'url'],
+            'principal_name'     => ['required', 'string'],
+            'signature'          => ['nullable', 'string'],
+            'service_type_info'  => ['required', 'integer'],
+            'verify_type_info'   => ['required', 'integer'],
+            'deleted_at'         => ['nullable', 'datetime'],
+            'funcscope_category' => ['nullable', 'json'],
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'app_id'             => ['required', 'string', 'min:18', 'alpha_dash'],
+            'refresh_token'      => ['required', 'string'],
+            'nick_name'          => ['required', 'string'],
+            'head_img'           => ['required', 'url'],
+            'user_name'          => ['required', 'string'],
+            'alias'              => ['nullable', 'string'],
+            'qrcode_url'         => ['required', 'url'],
+            'principal_name'     => ['required', 'string'],
+            'signature'          => ['nullable', 'string'],
+            'service_type_info'  => ['required', 'integer'],
+            'verify_type_info'   => ['required', 'integer'],
+            'deleted_at'         => ['nullable', 'datetime'],
+            'funcscope_category' => ['nullable', 'json'],
+        ],
     ];
 }
