943650b45bae85b949fc64a6bf5d8801c1b9b2c5
githubfisher <gooodfisher@126.com>
2019-11-22 13:50:04 +0800

    chore:fillValidator

--- app/Validators/Reply/WeRuleValidator.php
+++ app/Validators/Reply/WeRuleValidator.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Validators\Reply;
 
-use \Prettus\Validator\Contracts\ValidatorInterface;
 use \Prettus\Validator\LaravelValidator;
+use \Prettus\Validator\Contracts\ValidatorInterface;
 
 /**
  * Class RuleValidator.
@@ -18,7 +17,21 @@ class WeRuleValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'app_id'     => ['required', 'string', 'min:18', 'alpha_dash'],
+            'scene'      => ['required', 'string'],
+            'title'      => ['required', 'string'],
+            'reply_rule' => ['required', 'integer', 'in:1,2'],
+            'start_at'   => ['nullable', 'datetime'],
+            'end_at'     => ['nullable', 'datetime'],
+            'status'     => ['required', 'integer', 'in:0,1'],
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'title'      => ['sometimes', 'required', 'string'],
+            'reply_rule' => ['sometimes', 'required', 'integer', 'in:1,2'],
+            'start_at'   => ['sometimes', 'nullable', 'datetime'],
+            'end_at'     => ['sometimes', 'nullable', 'datetime'],
+            'status'     => ['sometimes', 'required', 'integer', 'in:0,1'],
+        ],
     ];
 }
