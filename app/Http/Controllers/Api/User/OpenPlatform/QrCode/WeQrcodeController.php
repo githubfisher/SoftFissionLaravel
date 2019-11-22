dbe0ec268f32618da0dc94fc1aa3121bfbc96429
githubfisher <gooodfisher@126.com>
2019-11-22 09:30:11 +0800

    chore:fixFuncName

--- app/Http/Controllers/Api/User/OpenPlatform/QrCode/WeQrcodeController.php
+++ app/Http/Controllers/Api/User/OpenPlatform/QrCode/WeQrcodeController.php
@@ -34,7 +34,7 @@ class WeQrcodeController extends Controller
         $this->authorize('view', WeQrcode::class);
 
         $limit = $request->input('limit', Constant::PAGINATE_MIN);
-        $list  = $this->repository->local($this->user()->id)->app($request->input('app_id'))->recent()->simplePaginate($limit);
+        $list  = $this->repository->user($this->user()->id)->app($request->input('app_id'))->recent()->simplePaginate($limit);
 
         return $this->suc(compact('list'));
     }
@@ -75,7 +75,7 @@ class WeQrcodeController extends Controller
     {
         $this->authorize('view', WeQrcode::class);
 
-        $data = $this->repository->get($id);
+        $data = $this->repository->with('details')->find($id);
 
         return $this->suc(compact('data'));
     }
