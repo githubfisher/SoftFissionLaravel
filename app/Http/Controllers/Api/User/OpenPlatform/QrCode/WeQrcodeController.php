732fc58eedd085b9239e84c50af7fbbd18ce3902
githubfisher <gooodfisher@126.com>
2019-11-22 15:21:01 +0800

    chore:changeFuncSort

--- app/Http/Controllers/Api/User/OpenPlatform/QrCode/WeQrcodeController.php
+++ app/Http/Controllers/Api/User/OpenPlatform/QrCode/WeQrcodeController.php
@@ -40,7 +40,7 @@ class WeQrcodeController extends Controller
         $this->authorize('view', WeQrcode::class);
 
         $limit = $request->input('limit', Constant::PAGINATE_MIN);
-        $list  = $this->repository->recent()->app($request->input('app_id'))->simplePaginate($limit);
+        $list  = $this->repository->app($request->input('app_id'))->recent()->simplePaginate($limit);
 
         return $this->suc(compact('list'));
     }
