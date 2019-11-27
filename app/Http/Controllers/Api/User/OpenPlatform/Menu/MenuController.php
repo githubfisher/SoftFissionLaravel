86aa5e8d69644ea096484a8dffced23c8519e55c
githubfisher <gooodfisher@126.com>
2019-11-27 13:30:27 +0800

    vardump menu list

--- app/Http/Controllers/Api/User/OpenPlatform/Menu/MenuController.php
+++ app/Http/Controllers/Api/User/OpenPlatform/Menu/MenuController.php
@@ -28,6 +28,7 @@ class MenuController extends Controller
         $this->authorize('view', WeMenu::class);
 
         $list          = $this->repository->app(current_weapp()['app_id'])->with(['details', 'details.rule', 'details.rule.replies'])->get();
+        dd($list);
         //$list && $list = $this->repository->sortBtns($list);
 
         return $this->suc(compact('list'));
