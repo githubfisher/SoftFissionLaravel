c51e4e49121576ecb7cdbeb1a8cfaa35e6b0caac
githubfisher <gooodfisher@126.com>
2019-11-27 14:10:33 +0800

    fill validator

--- app/Validators/Menu/WeMenuDetailValidator.php
+++ app/Validators/Menu/WeMenuDetailValidator.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Validators\Menu;
 
-use \Prettus\Validator\Contracts\ValidatorInterface;
 use \Prettus\Validator\LaravelValidator;
+use \Prettus\Validator\Contracts\ValidatorInterface;
 
 /**
  * Class WeMenuDetailValidator.
@@ -18,7 +17,19 @@ class WeMenuDetailValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'name'    => ['required', 'string'],
+            'menu_id' => ['required', 'integer', 'min:1'],
+            'pid'     => ['sometimes', 'integer', 'min:0'],
+            'rule_id' => ['sometimes', 'integer', 'min:0'],
+            'status'  => ['sometimes', 'integer', 'in:0,1'],
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'name'    => ['sometimes', 'string'],
+            'menu_id' => ['sometimes', 'integer', 'min:1'],
+            'pid'     => ['sometimes', 'integer', 'min:0'],
+            'rule_id' => ['sometimes', 'integer', 'min:0'],
+            'status'  => ['sometimes', 'integer', 'in:0,1'],
+        ],
     ];
 }
