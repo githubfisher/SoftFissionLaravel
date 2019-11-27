22ae246271ba3a70285470af2760784b8a3fe888
githubfisher <gooodfisher@126.com>
2019-11-27 14:04:57 +0800

    fix how to use func

--- app/Repositories/Menu/WeMenuRepositoryEloquent.php
+++ app/Repositories/Menu/WeMenuRepositoryEloquent.php
@@ -205,7 +205,7 @@ class WeMenuRepositoryEloquent extends BaseRepository implements CacheableInterf
             // 取旧菜单设置
             $list                   = $this->app($params['appInfo']['app_id'])->with(['details', 'details.rule', 'details.rule.replies'])->get();
             $list                   = $list->toArray();
-            ! empty($list) && $list = $this->repository->sortBtns($list);
+            ! empty($list) && $list = $this->sortBtns($list);
 
             $ruleRepository   = app()->make(WeRuleRepositoryEloquent::class);
             $detailRepository = app()->make(WeMenuDetailRepositoryEloquent::class);
