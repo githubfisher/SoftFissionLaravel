7d55930a2342d4218761fb7d99b34564c329aec3
githubfisher <gooodfisher@126.com>
2019-11-28 16:59:37 +0800

    fill shop_brands models

--- app/Entities/Shop/ShopsBrands.php
+++ app/Entities/Shop/ShopsBrands.php
@@ -2,6 +2,7 @@
 namespace App\Entities\Shop;
 
 use App\Entities\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * Class Shop.
@@ -10,4 +11,37 @@ use App\Entities\Model;
  */
 class ShopsBrands extends Model
 {
+    /**
+     * 可以被批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'shop_id',
+        'brand_id',
+    ];
+
+    /**
+     * 数组中的属性会被隐藏。
+     *
+     * @var array
+     */
+    protected $hidden  = [];
+
+    /**
+     * 不可批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * 获取店铺
+     *
+     * @return BelongsToMany
+     */
+    public function shops(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Entities\Shop\Shop', 'shops_brands', 'brand_id', 'shop_id');
+    }
 }
